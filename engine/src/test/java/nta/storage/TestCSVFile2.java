e72adbb29fe296292e73df35ab5a239252ba8888
Haemi Yang <haemiyang@korea.ac.kr>
2011-12-22 15:24:24 +0900

    TAJO-272: CSV 파일을 Split 가능하게 개선

---
+++ engine/src/test/java/nta/storage/TestCSVFile2.java
@@ -0,0 +1,80 @@
+package nta.storage;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import nta.catalog.Schema;
+import nta.catalog.TableMeta;
+import nta.catalog.proto.TableProtos.DataType;
+import nta.catalog.proto.TableProtos.StoreType;
+import nta.catalog.proto.TableProtos.TableType;
+import nta.conf.NtaConf;
+import nta.engine.EngineTestingUtils;
+import nta.engine.NtaEngineConstants;
+
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocalFileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Before;	
+import org.junit.Test;
+
+public class TestCSVFile2 {
+	
+	private NtaConf conf;
+	private static String TEST_PATH = "target/test-data/TestCSVFile2";
+	
+	@Before
+	public void setup() throws Exception {
+		conf = new NtaConf();
+		conf.set(NtaEngineConstants.ENGINE_DATA_DIR, TEST_PATH);
+		EngineTestingUtils.buildTestDir(TEST_PATH);
+	}
+	
+	@Test
+	public void test() throws IOException {
+		Schema schema = new Schema();
+		schema.addColumn("string", DataType.STRING);
+		schema.addColumn("int", DataType.INT);
+		
+		TableMeta meta = new TableMeta();
+		meta.setSchema(schema);
+		meta.setStorageType(StoreType.CSV);
+		meta.setTableType(TableType.BASETABLE);
+		
+		Path path = new Path(TEST_PATH);
+
+		Appender appender = new CSVFile2.CSVAppender(conf, path, schema);
+		int tupleNum = 10000;
+		VTuple vTuple = null;
+		for(int i = 0; i < tupleNum; i++) {
+			vTuple = new VTuple(2);
+			vTuple.put(0, "abc");
+			vTuple.put(1, (Integer)(i+1));
+			appender.addTuple(vTuple);
+		}
+		appender.close();
+
+		FileSystem fs = LocalFileSystem.get(conf);
+		FileStatus status = fs.getFileStatus(new Path(path, "data/table1.csv"));
+		long fileLen = status.getLen();		// 88894
+		long randomNum = (long) (Math.random() * fileLen) + 1;
+		
+		FileScanner fileScanner = new CSVFile2.CSVScanner(conf, path, schema, 0, randomNum);
+		int tupleCnt;
+		vTuple = null;
+		vTuple = (VTuple) fileScanner.next();
+		for (tupleCnt = 0; vTuple != null; tupleCnt++) {
+			vTuple = (VTuple) fileScanner.next();			
+		}
+		
+		fileScanner = new CSVFile2.CSVScanner(conf, path, schema, randomNum, fileLen);
+		for (; vTuple != null; tupleCnt++) {
+			vTuple = (VTuple) fileScanner.next();
+		}		
+		fileScanner.close();
+		
+		assertEquals(tupleCnt, tupleNum);
+	}
+}
