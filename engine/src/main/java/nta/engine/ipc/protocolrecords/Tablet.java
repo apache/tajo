c3a7ae26b1044f159383341890f37211eed50544
Jihoon Son <jihoonson@korea.ac.kr>
2011-12-29 19:26:30 +0900

    TAJO-295: RawFile2 클래스가 여러 파일로 구성된 테이블을 읽고 쓸 수 있도록 수정

--- engine/src/main/java/nta/engine/ipc/protocolrecords/Tablet.java
+++ engine/src/main/java/nta/engine/ipc/protocolrecords/Tablet.java
@@ -1,21 +1,30 @@
 package nta.engine.ipc.protocolrecords;
 
+import org.apache.hadoop.fs.Path;
+
+
 public class Tablet implements Comparable<Tablet> {
 	
 	private long startOffset;
 	private long length;
+	private Path path;
 	
 	public Tablet() {
 		
 	}
 	
-	public Tablet(long start, long length) {
-		this.set(start, length);
+	public Tablet(Path path, long start, long length) {
+		this.set(path, start, length);
 	}
 	
-	public void set(long start, long length) {
+	public void set(Path path, long start, long length) {
+		this.path = path;
 		this.startOffset = start;
 		this.length = length;
+	}
+	
+	public Path getPath() {
+		return this.path;
 	}
 	
 	public long getStartOffset() {
@@ -27,12 +36,35 @@ public class Tablet implements Comparable<Tablet> {
 	}
 	
 	/**
+	 * 
 	 * The offset range of tablets <b>MUST NOT</b> be overlapped.
 	 * @param t
-	 * @return
+	 * @return If the table paths are not same, return -1. 
 	 */
 	@Override
 	public int compareTo(Tablet t) {
-		return (int)(this.startOffset - t.getStartOffset());
+		if (t.getPath().equals(this.path)) {
+			return (int)(this.startOffset - t.getStartOffset());
+		} else {
+			return -1;
+		}
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Tablet) {
+			Tablet t = (Tablet)o;
+			if (t.getPath().equals(this.path)
+					&& t.getStartOffset() == this.startOffset
+					&& t.getLength() == this.length) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return new String("(" + path + " " + startOffset + " " + length + ")");
 	}
 }
