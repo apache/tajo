46fd6ec7396302532801972a13d7c60d6b338522
Jihoon Son <jihoonson@korea.ac.kr>
2011-12-29 14:18:04 +0900

    TAJO-299: FileScanner가 여러 파일을 읽을 수 있도록 수정. tablet의 배열을 입력 받음

--- engine/src/main/java/nta/engine/ipc/protocolrecords/Tablet.java
+++ engine/src/main/java/nta/engine/ipc/protocolrecords/Tablet.java
@@ -1,6 +1,38 @@
 package nta.engine.ipc.protocolrecords;
 
-public interface Tablet {
-	public long startOffset();
-	public long length();
+public class Tablet implements Comparable<Tablet> {
+	
+	private long startOffset;
+	private long length;
+	
+	public Tablet() {
+		
+	}
+	
+	public Tablet(long start, long length) {
+		this.set(start, length);
+	}
+	
+	public void set(long start, long length) {
+		this.startOffset = start;
+		this.length = length;
+	}
+	
+	public long getStartOffset() {
+		return this.startOffset;
+	}
+	
+	public long getLength() {
+		return this.length;
+	}
+	
+	/**
+	 * The offset range of tablets <b>MUST NOT</b> be overlapped.
+	 * @param t
+	 * @return
+	 */
+	@Override
+	public int compareTo(Tablet t) {
+		return (int)(this.startOffset - t.getStartOffset());
+	}
 }
