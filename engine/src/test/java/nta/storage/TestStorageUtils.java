ac796b8586f1ceb3844dfa11623a27a8af263bf4
Jimin Kim <jiminkim@korea.ac.kr>
2012-01-02 13:42:13 +0900

    TAJO-311: path로 tablet을 만들어주는 utility 클래스 추가

---
+++ engine/src/test/java/nta/storage/TestStorageUtils.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package nta.storage;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import nta.catalog.Schema;
+import nta.catalog.TableMeta;
+import nta.catalog.proto.TableProtos.DataType;
+import nta.catalog.proto.TableProtos.StoreType;
+import nta.catalog.proto.TableProtos.TableType;
+import nta.conf.NtaConf;
+import nta.engine.EngineTestingUtils;
+import nta.engine.NConstants;
+import nta.storage.Appender;
+import nta.storage.CSVFile2;
+import nta.storage.FileScanner;
+import nta.storage.StorageUtils;
+import nta.storage.VTuple;
+
+import org.apache.hadoop.fs.Path;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jimin Kim
+ * 
+ */
+public class TestStorageUtils {
+
+  private NtaConf conf;
+  private static String TEST_PATH = "target/test-data/TestStorageUtil";
+
+  int tupleNum = 10000;
+  Schema schema = null;
+  Path path = null;
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    conf = new NtaConf();
+    conf.set(NConstants.ENGINE_DATA_DIR, TEST_PATH);
+    EngineTestingUtils.buildTestDir(TEST_PATH);
+
+    schema = new Schema();
+    schema.addColumn("string", DataType.STRING);
+    schema.addColumn("int", DataType.INT);
+    
+
+    TableMeta meta = new TableMeta();
+    meta.setSchema(schema);
+    meta.setStorageType(StoreType.CSV);
+    meta.setTableType(TableType.BASETABLE);
+    
+    Path path = new Path(TEST_PATH);
+
+    path = new Path(TEST_PATH);
+
+    Appender appender = new CSVFile2.CSVAppender(conf, path, schema);
+    int tupleNum = 10000;
+
+    VTuple vTuple = null;
+    for (int i = 0; i < tupleNum; i++) {
+      vTuple = new VTuple(2);
+      vTuple.put(0, "abc");
+      vTuple.put(1, (Integer)(i+1));
+      vTuple.put(1, (Integer) (i + 1));
+      appender.addTuple(vTuple);
+    }
+    appender.close();
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void test() throws IOException {
+    FileScanner csvscanner = new CSVFile2.CSVScanner(conf, schema, StorageUtils.reconstructTablets(conf, new Path(TEST_PATH)));
+
+    int tupleCnt = 0;
+    VTuple vTuple = null;
+    while ((vTuple = (VTuple) csvscanner.next()) != null) {
+      tupleCnt++;
+    }
+
+    assertEquals(tupleNum, tupleCnt);
+  }
+
+}
