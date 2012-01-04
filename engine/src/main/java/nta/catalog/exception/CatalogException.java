3cff4a325f12325c9d6892c9859977a6f5cc5c7b
Hyunsik Choi <hyunsik.choi@gmail.com>
2012-01-05 03:40:51 +0900

    TAJO-301: CatalogService Interface 구현 https://dbserver.korea.ac.kr/jira/browse/TAJO-301

---
+++ engine/src/main/java/nta/catalog/exception/CatalogException.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package nta.catalog.exception;
+
+import java.io.IOException;
+
+/**
+ * @author hyunsik
+ *
+ */
+public class CatalogException extends Throwable {
+  private static final long serialVersionUID = -26362412527118618L;
+
+  /**
+   * 
+   */
+  public CatalogException() {
+  }
+
+  /**
+   * @param message
+   */
+  public CatalogException(String message) {
+    super(message);
+  }
+
+  /**
+   * @param cause
+   */
+  public CatalogException(Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * @param message
+   * @param cause
+   */
+  public CatalogException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
