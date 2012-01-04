8b76f2f45cac7b2125bfba952a4386fa6be936bf
Jihoon Son <jihoonson@korea.ac.kr>
2012-01-04 18:17:19 +0900

    TAJO-316: NQL 클래스로부터 RelInfo 클래스 분리 완료 및 RelInfo 클래스를 사용하는 클래스들의 import path 변경 완료

--- engine/src/main/java/nta/engine/parser/NQL.java
+++ engine/src/main/java/nta/engine/parser/NQL.java
@@ -465,53 +465,6 @@ public class NQL {
 	
 	
 	
-	public class RelInfo implements SchemaObject {
-		public String alias;
-		public TableInfo rel;
-		
-		public RelInfo(TableInfo rel) {
-			this.rel = rel;
-		}
-		
-		public RelInfo(TableInfo rel, String alias) {
-			this(rel);
-			this.alias = alias;
-		}
-		
-		public void setTableDesc(TableInfo rel) {
-			this.rel = rel;
-		}
-		
-		public boolean hasAlias() {
-			return alias != null;
-		}
-		
-		public Schema getSchema() {
-			return this.rel.getSchema();
-		}
-		
-		public TableInfo getRelation() {
-			return this.rel;
-		}
-		
-		public String alias() {
-			return this.alias;
-		}
-		
-		public String getName() {
-			return rel.getName();
-		}
-		
-		public String toString() {
-			StringBuilder sb = new StringBuilder();
-			sb.append(this.rel);			
-			if(hasAlias()) {
-				sb.append("("+alias+")");
-			}
-			return sb.toString();
-		}
-	}
-	
 	public class Query {
 		private String queryString;
 		private CommonTree ast;
@@ -522,7 +475,7 @@ public class NQL {
 		// FROM
 		boolean hasFromClause = false;
 		public int numBaseRels;
-		public Map<String,RelInfo> tableMap = new HashMap<String, NQL.RelInfo>();	
+		public Map<String,RelInfo> tableMap = new HashMap<String, RelInfo>();	
 		
 		// Join
 		boolean hasJoinQual = false;		
