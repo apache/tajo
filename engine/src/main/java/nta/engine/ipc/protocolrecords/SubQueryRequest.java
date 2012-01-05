572635731b9df583e9fdfd65f188854a2d4b82c0
Jihoon Son <jihoonson@korea.ac.kr>
2012-01-05 13:13:08 +0900

    TAJO-317: SubQueryRequest 인터페이스의 query 타입을 String에서 Query로 변환

--- engine/src/main/java/nta/engine/ipc/protocolrecords/SubQueryRequest.java
+++ engine/src/main/java/nta/engine/ipc/protocolrecords/SubQueryRequest.java
@@ -3,13 +3,15 @@ package nta.engine.ipc.protocolrecords;
 import java.net.URI;
 import java.util.List;
 
+import nta.engine.parser.NQL.Query;
+
 /**
  * This contains the rewrote query and its global query plan. 
  * 
  * @author hyunsik
  */
 public interface SubQueryRequest {
-  public String getQuery();
+  public Query getQuery();
 
   public List<Tablet> getTablets();
 
