7fa5368efac7e8dd1d9d37c0d374d261657a67a4
Choi Daejin <soyenze@Choi-Daejins-MacBook-Pro.local>
2012-02-01 15:15:48 +0900

    TAJO-425:통신에 사용되는 모든 타입을 이용한 RPC 테스트

--- rpc/src/test/java/nta/rpc/TestProtoParamAsyncRpc.java
+++ rpc/src/test/java/nta/rpc/TestProtoParamAsyncRpc.java
@@ -9,8 +9,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-import nta.rpc.test.DummyProtos.MulRequest;
+import nta.rpc.test.DummyProtos.MulRequest1;
+import nta.rpc.test.DummyProtos.MulRequest2;
 import nta.rpc.test.DummyProtos.MulResponse;
+import nta.rpc.test.DummyProtos.InnerRequest;
+import nta.rpc.test.DummyProtos.InnerResponse;
+import nta.rpc.test.DummyProtos.InnerNode;
 
 public class TestProtoParamAsyncRpc {
   public static String MESSAGE = TestProtoParamAsyncRpc.class.getName();
@@ -18,43 +22,56 @@ public class TestProtoParamAsyncRpc {
   DummyClientInterface proxy;
 
   public static interface DummyServerInterface {
-    public MulResponse mul(MulRequest request);
-
-    public Object throwException(MulRequest request) throws IOException;
-
-    public Integer primitiveTypeTest(String inStr);
+    public Object throwException(MulRequest1 request) throws IOException;
+    public MulResponse mul(MulRequest1 req1, MulRequest2 req2);
+    public Integer primitiveTypeTest(String inStr, int value);
+    public InnerResponse innerProtocolbufferTest(InnerRequest in);
   }
 
   public static interface DummyClientInterface {
-    public void mul(Callback<MulResponse> callback, MulRequest request);
-
-    public void throwException(Callback<Object> callback, MulRequest request)
+    public void throwException(Callback<Object> callback, MulRequest1 request)
         throws IOException;
-
-    public void primitiveTypeTest(Callback<Integer> callback, String inStr);
+    public void mul(Callback<MulResponse> callback, MulRequest1 req1, MulRequest2 req2);
+    public void primitiveTypeTest(Callback<Integer> callback, String inStr, int value);
+    public void innerProtocolbufferTest(Callback<InnerResponse> res, InnerRequest in);
   }
 
   public static class DummyServer implements DummyServerInterface {
     @Override
-    public MulResponse mul(MulRequest request) {
-      int x1 = request.getX1();
-      int x2 = request.getX2();
-
-      int result = x1 * x2;
-
-      MulResponse rst = MulResponse.newBuilder().setResult(result).build();
-      return rst;
-    }
-
-    @Override
-    public Object throwException(MulRequest request) throws IOException {
+    public Object throwException(MulRequest1 request) throws IOException {
       throw new IOException();
     }
 
     @Override
-    public Integer primitiveTypeTest(String inStr) {
+    public MulResponse mul(MulRequest1 req1, MulRequest2 req2) {
+      int x1_1 = req1.getX1();
+      int x1_2 = req1.getX2();
+      int x2_1 = req2.getX1();
+      int x2_2 = req2.getX2();
+
+      int result1 = x1_1 * x2_1;
+      int result2 = x1_2 * x2_2;
+
+      MulResponse rst = MulResponse.newBuilder().setResult1(result1).setResult2(result2).build();
+      return rst;
+    }
+
+    @Override
+    public Integer primitiveTypeTest(String inStr, int value) {
       System.out.println("Server" + inStr);
-      return 10;
+      return value;
+    }
+    
+    @Override
+    public InnerResponse innerProtocolbufferTest(InnerRequest in) {
+      InnerResponse res = null;
+      InnerResponse.Builder builder = InnerResponse.newBuilder();
+      for( InnerNode node : in.getNodesList() ) {
+        builder.addNodes(node);
+      }
+      res = builder.build();
+      return res;
+      
     }
   }
 
@@ -80,18 +97,38 @@ public class TestProtoParamAsyncRpc {
   MulResponse answer1 = null;
 
   @Test
+  public void testRpcRemoteException() throws Exception {
+
+    MulRequest1 req = MulRequest1.newBuilder().setX1(10).setX2(20).build();
+
+    System.out.println("Do whatever you want before get result!!");
+
+    try {
+      Callback<Object> cb = new Callback<Object>();
+      proxy.throwException(cb, req);
+      cb.get();
+
+    } catch (RemoteException e) {
+      System.out.println(e.getMessage());
+    }
+
+  }
+
+  @Test
   public void testRpcProtoType() throws Exception {
 
-    MulRequest req = MulRequest.newBuilder().setX1(10).setX2(20).build();
+    MulRequest1 req1 = MulRequest1.newBuilder().setX1(10).setX2(20).build();
+    MulRequest2 req2 = MulRequest2.newBuilder().setX1(10).setX2(20).build();
 
     System.out.println("Do whatever you want before get result!!");
 
     try {
       Callback<MulResponse> cb = new Callback<MulResponse>();
-      proxy.mul(cb, req);
+      proxy.mul(cb, req1, req2);
 
       MulResponse resp = (MulResponse) cb.get();
-      assertEquals(200, resp.getResult());
+      assertEquals(100, resp.getResult1());
+      assertEquals(400, resp.getResult2());
 
     } catch (RemoteException e) {
       System.out.println(e.getMessage());
@@ -105,7 +142,7 @@ public class TestProtoParamAsyncRpc {
 
     try {
       Callback<Integer> cb = new Callback<Integer>();
-      proxy.primitiveTypeTest(cb, "TEST STRING");
+      proxy.primitiveTypeTest(cb, "TEST STRING", 10);
 
       Integer x = cb.get();
       System.out.println(x);
@@ -113,25 +150,28 @@ public class TestProtoParamAsyncRpc {
     } catch (RemoteException e) {
       System.out.println(e.getMessage());
     }
-
   }
 
   @Test
-  public void testRpcRemoteException() throws Exception {
-
-    MulRequest req = MulRequest.newBuilder().setX1(10).setX2(20).build();
-
-    System.out.println("Do whatever you want before get result!!");
+  public void testInnerProtocolBuffer() throws Exception {
+    String strs[] = {"SUN", "MON", "TUE", "WED", "THR", "FRI", "SAT"};
+    InnerRequest.Builder builder = InnerRequest.newBuilder();
 
     try {
-      Callback<Object> cb = new Callback<Object>();
-      proxy.throwException(cb, req);
-      cb.get();
-
+      for( String str : strs) {
+        InnerNode node = InnerNode.newBuilder().setInstr(str).build();
+        builder.addNodes(node);
+      }
+      InnerRequest req = builder.build();
+      Callback<InnerResponse> res = new Callback<InnerResponse>();
+      proxy.innerProtocolbufferTest(res, req);
+      InnerResponse nodeList = (InnerResponse)res.get();
+      for( InnerNode node :nodeList.getNodesList() ) {
+        System.out.println(node.getInstr());
+      }
     } catch (RemoteException e) {
       System.out.println(e.getMessage());
     }
-
   }
 
 }
