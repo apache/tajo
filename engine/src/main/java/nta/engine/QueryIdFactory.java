44a233acff81745aa55edc817fe53b58ed2950e6
jihoonson <jihoonson@korea.ac.kr>
2012-02-18 13:30:57 +0900

    TAJO-530: QueryIdFactory가 concurrency를 지원하도록 개선
    Change-Id: I213f05ce04112eb3cdc3ede2c87642cc02f775e2
    Reviewed-on: https://dbserver.korea.ac.kr/reviews/47
    Tested-by:  Jenkins <build@dbserver.korea.ac.kr>
    Reviewed-by:  jihoon <jihoonson@korea.ac.kr>

--- engine/src/main/java/nta/engine/QueryIdFactory.java
+++ engine/src/main/java/nta/engine/QueryIdFactory.java
@@ -5,6 +5,7 @@ package nta.engine;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author jihoon
@@ -16,49 +17,56 @@ public class QueryIdFactory {
   private static SubQueryId subQueryId;
   private static QueryStepId queryStepId;
   private static QueryUnitId queryUnitId;
-  private static int nextQueryId = -1;
-  private static int nextSubQueryId = -1;
-  private static int nextQueryStepId = -1;
-  private static int nextQueryUnitId = -1;
+  private static AtomicInteger nextQueryId= new AtomicInteger(-1);
+  private static AtomicInteger nextSubQueryId = new AtomicInteger(-1);
+  private static AtomicInteger nextQueryStepId = new AtomicInteger(-1);
+  private static AtomicInteger nextQueryUnitId = new AtomicInteger(-1);
   
   public static void reset() {
     Date dateNow = new Date();
     SimpleDateFormat dateformatYYYYMMDD = new SimpleDateFormat("yyyyMMddSS");
     timeId = dateformatYYYYMMDD.format(dateNow);
-    nextQueryId = -1;
-    nextSubQueryId = -1;
-    nextQueryStepId = -1;
-    nextQueryUnitId = -1;
+    nextQueryId.set(-1); 
+    nextSubQueryId.set(-1);
+    nextQueryStepId.set(-1);
+    nextQueryUnitId.set(-1);
   }
 
   public static QueryId newQueryId() {
-    queryId = new QueryId(timeId, ++nextQueryId);
-    nextSubQueryId = -1;
+    queryId = new QueryId(timeId, nextQueryId.incrementAndGet());
+    nextSubQueryId.set(-1);
     return queryId;
   }
   
   public static SubQueryId newSubQueryId() {
-    if (nextQueryId == -1) {
+    if (nextQueryId.equals(-1)) {
       newQueryId();
     }
-    subQueryId = new SubQueryId(queryId, ++nextSubQueryId);
-    nextQueryUnitId = -1;
+    subQueryId = new SubQueryId(queryId, nextSubQueryId.incrementAndGet());
+    nextQueryUnitId.set(-1);
     return subQueryId;
   }
   
   public static QueryUnitId newQueryUnitId() {
-    if (nextQueryStepId == -1) {
+    if (nextQueryStepId.equals(-1)) {
       newQueryStepId();
     }
-    queryUnitId = new QueryUnitId(queryStepId, ++nextQueryUnitId);
+    queryUnitId = new QueryUnitId(queryStepId, nextQueryUnitId.incrementAndGet());
     return queryUnitId;
   }
   
   public static QueryStepId newQueryStepId() {
-    if (nextSubQueryId == -1) {
+    if (nextSubQueryId.equals(-1)) {
       newSubQueryId();
     }
-    queryStepId = new QueryStepId(subQueryId, ++nextQueryStepId);
+    queryStepId = new QueryStepId(subQueryId, nextQueryStepId.incrementAndGet());
     return queryStepId;
+  }
+  
+  public static String nextQueryStepId() {
+    if (nextSubQueryId.equals(-1)) {
+      newSubQueryId();
+    }
+    return subQueryId.toString() + "_" + nextQueryStepId.incrementAndGet();
   }
 }
