b7e5c703f58ea61489ec0c1a57212bf2a12345b7
Hyunsik Choi <hyunsik.choi@gmail.com>
2012-06-11 22:13:34 +0900

    TAJO-768: The prefix 'TAZO' in tajo-env.sh should be change 'TAJO'
    Change-Id: I7938179a0a6ae85401e61ffa83bdf9a5252c64a4
    Reviewed-on: https://dbserver.korea.ac.kr/reviews/206
    Tested-by:  Jenkins <build@dbserver.korea.ac.kr>
    Reviewed-by: Hyunsik Choi <hyunsik.choi@gmail.com>

--- engine/conf/tajo-env.sh
+++ engine/conf/tajo-env.sh
@@ -9,47 +9,47 @@
 export JAVA_HOME=/usr
 
 # Extra Java CLASSPATH elements.  Optional.
-# export TAZO_CLASSPATH=
+# export TAJO_CLASSPATH=
 
 # The maximum amount of heap to use, in MB. Default is 1000.
-# export TAZO_HEAPSIZE=2000
+export TAJO_HEAPSIZE=12000
 
 # Extra Java runtime options.  Empty by default.
-# export TAZO_OPTS=-server
+# export TAJO_OPTS=-server
 
-# Command specific options appended to TAZO_OPTS when specified
-#export TAZO_NAMENODE_OPTS="-Dcom.sun.management.jmxremote $TAZO_NAMENODE_OPTS"
-#export TAZO_SECONDARYNAMENODE_OPTS="-Dcom.sun.management.jmxremote $TAZO_SECONDARYNAMENODE_OPTS"
-#export TAZO_DATANODE_OPTS="-Dcom.sun.management.jmxremote $TAZO_DATANODE_OPTS"
-#export TAZO_BALANCER_OPTS="-Dcom.sun.management.jmxremote $TAZO_BALANCER_OPTS"
-#export TAZO_JOBTRACKER_OPTS="-Dcom.sun.management.jmxremote $TAZO_JOBTRACKER_OPTS"
+# Command specific options appended to TAJO_OPTS when specified
+#export TAJO_NAMENODE_OPTS="-Dcom.sun.management.jmxremote $TAJO_NAMENODE_OPTS"
+#export TAJO_SECONDARYNAMENODE_OPTS="-Dcom.sun.management.jmxremote $TAJO_SECONDARYNAMENODE_OPTS"
+#export TAJO_DATANODE_OPTS="-Dcom.sun.management.jmxremote $TAJO_DATANODE_OPTS"
+#export TAJO_BALANCER_OPTS="-Dcom.sun.management.jmxremote $TAJO_BALANCER_OPTS"
+#export TAJO_JOBTRACKER_OPTS="-Dcom.sun.management.jmxremote $TAJO_JOBTRACKER_OPTS"
 
-# export TAZO_TASKTRACKER_OPTS=
+# export TAJO_TASKTRACKER_OPTS=
 # The following applies to multiple commands (fs, dfs, fsck, distcp etc)
-# export TAZO_CLIENT_OPTS
+# export TAJO_CLIENT_OPTS
 
 # Extra ssh options.  Empty by default.
-# export TAZO_SSH_OPTS="-o ConnectTimeout=1 -o SendEnv=TAZO_CONF_DIR"
+# export TAJO_SSH_OPTS="-o ConnectTimeout=1 -o SendEnv=TAJO_CONF_DIR"
 
-# Where log files are stored.  $TAZO_HOME/logs by default.
-# export TAZO_LOG_DIR=${TAZO_HOME}/logs
+# Where log files are stored.  $TAJO_HOME/logs by default.
+# export TAJO_LOG_DIR=${TAJO_HOME}/logs
 
-# File naming remote slave hosts.  $TAZO_HOME/conf/slaves by default.
-# export TAZO_SLAVES=${TAZO_HOME}/conf/slaves
+# File naming remote slave hosts.  $TAJO_HOME/conf/slaves by default.
+# export TAJO_SLAVES=${TAJO_HOME}/conf/slaves
 
 # host:path where hadoop code should be rsync'd from.  Unset by default.
-# export TAZO_MASTER=master:/home/$USER/src/hadoop
+# export TAJO_MASTER=master:/home/$USER/src/hadoop
 
 # Seconds to sleep between slave commands.  Unset by default.  This
 # can be useful in large clusters, where, e.g., slave rsyncs can
 # otherwise arrive faster than the master can service them.
-# export TAZO_SLAVE_SLEEP=0.1
+# export TAJO_SLAVE_SLEEP=0.1
 
 # The directory where pid files are stored. /tmp by default.
-# export TAZO_PID_DIR=/var/hadoop/pids
+# export TAJO_PID_DIR=/var/hadoop/pids
 
 # A string representing this instance of hadoop. $USER by default.
-# export TAZO_IDENT_STRING=$USER
+# export TAJO_IDENT_STRING=$USER
 
 # The scheduling priority for daemon processes.  See 'man nice'.
-# export TAZO_NICENESS=10
+# export TAJO_NICENESS=10
