2a26edffed8a309ab53f6d253b813e07fa53c245
jinossy <jinossy@gmail.com>
2013-09-10 10:43:18 +0900

    TAJO-174: SQL error handler sometimes occurred NPE. (jinho)

--- tajo-core/tajo-core-backend/src/main/java/org/apache/tajo/engine/query/exception/SQLParseError.java
+++ tajo-core/tajo-core-backend/src/main/java/org/apache/tajo/engine/query/exception/SQLParseError.java
@@ -91,10 +91,12 @@ public class SQLParseError extends RuntimeException {
       } else if (errorLine.length() - (locationString.length() + padding) <= 0) {
         startPos = errorLine.length() - displayLimit - 1;
         sb.append(ellipsis).append(errorLine.substring(startPos)).append("\n");
-        sb.append(prefixPadding).append(locationString.substring(startPos - ellipsis.length()));
+        sb.append(prefixPadding).append(StringUtils.repeat(" ", ellipsis.length()))
+            .append(locationString.substring(startPos));
       } else {
         sb.append(ellipsis).append(errorLine.substring(startPos, startPos + displayLimit)).append(ellipsis).append("\n");
-        sb.append(prefixPadding).append(locationString.substring(startPos - ellipsis.length()));
+        sb.append(prefixPadding).append(StringUtils.repeat(" ", ellipsis.length()))
+            .append(locationString.substring(startPos));
       }
     } else {
       sb.append(errorLine).append("\n");
