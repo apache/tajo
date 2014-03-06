462abc47113a5c41fed783c34cd5b35a8eae4758
Hyunsik Choi <hyunsik@apache.org>
2014-03-06 14:16:05 +0900

    TAJO-650: Repartitioner::scheduleHashShuffledFetches should adjust the number of tasks.

--- tajo-core/tajo-core-backend/src/main/java/org/apache/tajo/master/querymaster/Repartitioner.java
+++ tajo-core/tajo-core-backend/src/main/java/org/apache/tajo/master/querymaster/Repartitioner.java
@@ -369,11 +369,11 @@ public class Repartitioner {
     }
 
     boolean ascendingFirstKey = sortSpecs[0].isAscending();
-    SortedMap<TupleRange, Set<URI>> map;
+    SortedMap<TupleRange, Collection<URI>> map;
     if (ascendingFirstKey) {
-      map = new TreeMap<TupleRange, Set<URI>>();
+      map = new TreeMap<TupleRange, Collection<URI>>();
     } else {
-      map = new TreeMap<TupleRange, Set<URI>>(new TupleRange.DescendingTupleRangeComparator());
+      map = new TreeMap<TupleRange, Collection<URI>>(new TupleRange.DescendingTupleRangeComparator());
     }
 
     Set<URI> uris;
@@ -398,7 +398,7 @@ public class Repartitioner {
     schedulerContext.setEstimatedTaskNum(determinedTaskNum);
   }
 
-  public static void scheduleFetchesByRoundRobin(SubQuery subQuery, Map<?, Set<URI>> partitions,
+  public static void scheduleFetchesByRoundRobin(SubQuery subQuery, Map<?, Collection<URI>> partitions,
                                                    String tableName, int num) {
     int i;
     Map<String, List<URI>>[] fetchesArray = new Map[num];
@@ -406,9 +406,9 @@ public class Repartitioner {
       fetchesArray[i] = new HashMap<String, List<URI>>();
     }
     i = 0;
-    for (Entry<?, Set<URI>> entry : partitions.entrySet()) {
-      Set<URI> value = entry.getValue();
-      fetchesArray[i++].put(tableName, Lists.newArrayList(value));
+    for (Entry<?, Collection<URI>> entry : partitions.entrySet()) {
+      Collection<URI> value = entry.getValue();
+      TUtil.putCollectionToNestedList(fetchesArray[i++], tableName, value);
       if (i == num) i = 0;
     }
     for (Map<String, List<URI>> eachFetches : fetchesArray) {
@@ -451,7 +451,7 @@ public class Repartitioner {
     SubQuery.scheduleFragments(subQuery, fragments);
 
     Map<String, List<IntermediateEntry>> hashedByHost;
-    Map<Integer, List<URI>> finalFetchURI = new HashMap<Integer, List<URI>>();
+    Map<Integer, Collection<URI>> finalFetchURI = new HashMap<Integer, Collection<URI>>();
 
     for (ExecutionBlock block : masterPlan.getChilds(execBlock)) {
       List<IntermediateEntry> partitions = new ArrayList<IntermediateEntry>();
@@ -477,7 +477,7 @@ public class Repartitioner {
     }
 
     GroupbyNode groupby = PlannerUtil.findMostBottomNode(subQuery.getBlock().getPlan(), NodeType.GROUP_BY);
-    // the number of tasks cannot exceed the number of merged fetch uris.
+    // get a proper number of tasks
     int determinedTaskNum = Math.min(maxNum, finalFetchURI.size());
     LOG.info("ScheduleHashShuffledFetches - Max num=" + maxNum + ", finalFetchURI=" + finalFetchURI.size());
     if (groupby != null && groupby.getGroupingColumns().length == 0) {
@@ -485,14 +485,10 @@ public class Repartitioner {
       LOG.info("No Grouping Column - determinedTaskNum is set to 1");
     }
 
-    for (Entry<Integer, List<URI>> entry : finalFetchURI.entrySet()) {
-      List<URI> value = entry.getValue();
-      Map<String, List<URI>> fetches = new HashMap<String, List<URI>>();
-      fetches.put(scan.getTableName(), value);
-      SubQuery.scheduleFetches(subQuery, fetches);
-    }
-
+    // set the proper number of tasks to the estimated task num
     schedulerContext.setEstimatedTaskNum(determinedTaskNum);
+    // divide fetch uris into the the proper number of tasks in a round robin manner.
+    scheduleFetchesByRoundRobin(subQuery, finalFetchURI, scan.getTableName(), determinedTaskNum);
     LOG.info("DeterminedTaskNum : " + determinedTaskNum);
   }
 
