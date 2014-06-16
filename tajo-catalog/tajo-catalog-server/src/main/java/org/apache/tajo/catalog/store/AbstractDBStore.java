4bdd2773275747398e26a4025e4cc90d55b56d3a
Jinhang Choi <jinhang@linewalks.com>
2014-06-16 15:41:18 +0900

    TAJO-847: Supporting MariaDB-based Store, which is compatible with MySQL.

--- tajo-catalog/tajo-catalog-server/src/main/java/org/apache/tajo/catalog/store/AbstractDBStore.java
+++ tajo-catalog/tajo-catalog-server/src/main/java/org/apache/tajo/catalog/store/AbstractDBStore.java
@@ -153,8 +153,17 @@ public abstract class AbstractDBStore extends CatalogConstants implements Catalo
   }
 
   public Connection getConnection() {
+    boolean isValid = false;
+
     try {
-      boolean isValid = conn.isValid(100);
+      isValid = conn.isValid(100);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (NullPointerException e) {
+      LOG.info("Conn abortion when checking validity; Try to connect database");
+    }
+
+    try {
       if (!isValid) {
         CatalogUtil.closeQuietly(conn);
         conn = createConnection(conf);
