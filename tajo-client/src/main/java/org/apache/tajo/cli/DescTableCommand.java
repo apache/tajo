0905851881ee8ce4e04b5005c81e05ec5ff3a13a
jinossy <jinossy@gmail.com>
2014-07-04 01:16:47 +0900

    TAJO-902: Unicode delimiter does not work correctly

--- tajo-client/src/main/java/org/apache/tajo/cli/DescTableCommand.java
+++ tajo-client/src/main/java/org/apache/tajo/cli/DescTableCommand.java
@@ -18,6 +18,8 @@
 
 package org.apache.tajo.cli;
 
+import org.apache.commons.lang.CharUtils;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.tajo.catalog.Column;
 import org.apache.tajo.catalog.TableDesc;
 import org.apache.tajo.catalog.partition.PartitionMethodDesc;
@@ -84,8 +86,13 @@ public class DescTableCommand extends TajoShellCommand {
     }
     sb.append("Options: \n");
     for(Map.Entry<String, String> entry : desc.getMeta().toMap().entrySet()){
+      String value = entry.getValue();
+      String unescaped = StringEscapeUtils.unescapeJava(value);
+      if(unescaped.length() == 1 && CharUtils.isAsciiPrintable(unescaped.charAt(0))){
+        value = unescaped;
+      }
       sb.append("\t").append("'").append(entry.getKey()).append("'").append("=")
-          .append("'").append(entry.getValue()).append("'").append("\n");
+          .append("'").append(value).append("'").append("\n");
     }
     sb.append("\n");
     sb.append("schema: \n");
