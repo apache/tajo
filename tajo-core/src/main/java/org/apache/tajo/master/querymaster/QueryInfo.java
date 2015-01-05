cb9793b990f1c882e3371a44e6c3f28fe913c1a7
Hyunsik Choi <hyunsik@apache.org>
2015-01-05 17:44:14 +0900

    TAJO-1228: TajoClient should communicate with only TajoMaster without TajoWorker.
    Closes #317

--- tajo-core/src/main/java/org/apache/tajo/master/querymaster/QueryInfo.java
+++ tajo-core/src/main/java/org/apache/tajo/master/querymaster/QueryInfo.java
@@ -22,7 +22,9 @@ package org.apache.tajo.master.querymaster;
 import com.google.gson.annotations.Expose;
 import org.apache.tajo.QueryId;
 import org.apache.tajo.TajoProtos;
+import org.apache.tajo.catalog.TableDesc;
 import org.apache.tajo.engine.json.CoreGsonHelper;
+import org.apache.tajo.engine.query.QueryContext;
 import org.apache.tajo.ipc.ClientProtos.QueryInfoProto;
 import org.apache.tajo.json.GsonObject;
 import org.apache.tajo.util.TajoIdUtils;
@@ -31,15 +33,17 @@ import org.apache.tajo.util.history.History;
 public class QueryInfo implements GsonObject, History {
   private QueryId queryId;
   @Expose
+  private QueryContext context;
+  @Expose
   private String sql;
   @Expose
-  private TajoProtos.QueryState queryState;
+  private volatile TajoProtos.QueryState queryState;
   @Expose
-  private float progress;
+  private volatile float progress;
   @Expose
-  private long startTime;
+  private volatile long startTime;
   @Expose
-  private long finishTime;
+  private volatile  long finishTime;
   @Expose
   private String lastMessage;
   @Expose
@@ -52,23 +56,31 @@ public class QueryInfo implements GsonObject, History {
   private int queryMasterInfoPort;
   @Expose
   private String queryIdStr;
+  @Expose
+  private volatile TableDesc resultDesc;
 
   private String jsonExpr;
 
   public QueryInfo(QueryId queryId) {
-    this(queryId, null, null);
+    this(queryId, null, null, null);
   }
 
-  public QueryInfo(QueryId queryId, String sql, String jsonExpr) {
+  public QueryInfo(QueryId queryId, QueryContext queryContext, String sql, String jsonExpr) {
     this.queryId = queryId;
     this.queryIdStr = queryId.toString();
+    this.context = queryContext;
     this.sql = sql;
     this.jsonExpr = jsonExpr;
+
     this.queryState = TajoProtos.QueryState.QUERY_MASTER_INIT;
   }
 
   public QueryId getQueryId() {
     return queryId;
+  }
+
+  public QueryContext getQueryContext() {
+    return context;
   }
 
   public String getSql() {
@@ -147,6 +159,18 @@ public class QueryInfo implements GsonObject, History {
     this.progress = progress;
   }
 
+  public void setResultDesc(TableDesc result) {
+    this.resultDesc = result;
+  }
+
+  public boolean hasResultdesc() {
+    return resultDesc != null;
+  }
+
+  public TableDesc getResultDesc() {
+    return resultDesc;
+  }
+
   @Override
   public String toString() {
     return queryId.toString() + ",state=" + queryState +",progress=" + progress + ", queryMaster="
@@ -182,12 +206,17 @@ public class QueryInfo implements GsonObject, History {
 
     builder.setQueryId(queryId.toString())
         .setQueryState(queryState)
+        .setContextVars(context.getProto())
         .setProgress(progress)
         .setStartTime(startTime)
         .setFinishTime(finishTime)
         .setQueryMasterPort(queryMasterPort)
         .setQueryMasterClientPort(queryMasterClientPort)
         .setQueryMasterInfoPort(queryMasterInfoPort);
+
+    if (resultDesc != null) {
+      builder.setResultDesc(resultDesc.getProto());
+    }
 
     if (sql != null) {
       builder.setSql(sql);
