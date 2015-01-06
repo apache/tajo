80c721c242fcfc66deceb23ab22fb7050570df22
JaeHwa Jung <blrunner@apache.org>
2015-01-06 11:37:31 +0900

    TAJO-1225: Fix wrong schema name in JDBC driver

--- tajo-common/src/main/java/org/apache/tajo/TajoConstants.java
+++ tajo-common/src/main/java/org/apache/tajo/TajoConstants.java
@@ -29,7 +29,7 @@ public class TajoConstants {
 
   public static final String DEFAULT_TABLESPACE_NAME = "default";
   public static final String DEFAULT_DATABASE_NAME = "default";
-  public static final String DEFAULT_SCHEMA_NAME = "public";
+  public static final String DEFAULT_SCHEMA_NAME = "";
 
   /** Java 6 only recognize GMT instead of UTC. So, we should keep using GMT. */
   public static final String DEFAULT_SYSTEM_TIMEZONE = "GMT";
