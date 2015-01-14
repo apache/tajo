194dddc5dc81d89bb9d34214c871520222b22834
jhkim <jhkim@apache.org>
2015-01-14 19:00:47 +0900

    fix the wrong close() ordering

--- tajo-storage/tajo-storage-hdfs/src/main/java/org/apache/tajo/storage/text/DelimitedTextFile.java
+++ tajo-storage/tajo-storage-hdfs/src/main/java/org/apache/tajo/storage/text/DelimitedTextFile.java
@@ -242,7 +242,7 @@ public class DelimitedTextFile {
           deflateFilter = null;
         }
       } finally {
-        IOUtils.cleanup(LOG, os, fos, outputStream);
+        IOUtils.cleanup(LOG, os, outputStream, fos);
         if (compressor != null) {
           CodecPool.returnCompressor(compressor);
           compressor = null;
