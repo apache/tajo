aa45f842146eb81c271a60d6fba4c97d61dc4a85
Jihoon Son <jihoonson@apache.org>
2015-02-09 20:26:46 +0900

    TAJO-1298

--- tajo-core/src/main/java/org/apache/tajo/engine/planner/physical/StoreIndexExec.java
+++ tajo-core/src/main/java/org/apache/tajo/engine/planner/physical/StoreIndexExec.java
@@ -66,7 +66,6 @@ public class StoreIndexExec extends UnaryPhysicalExec {
     }
 
     TajoConf conf = context.getConf();
-//    Path indexPath = new Path(logicalPlan.getIndexPath().toString(), context.getUniqueKeyFromFragments());
     Path indexPath = context.getOutputPath();
     // TODO: Create factory using reflection
     BSTIndex bst = new BSTIndex(conf);
