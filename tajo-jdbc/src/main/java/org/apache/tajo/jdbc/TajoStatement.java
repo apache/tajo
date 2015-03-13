b1d30b577ab68f8beac85ed447094c464064e938
HANYOUNSU <hanyounsu@HANYOUNSUui-MacBook-Pro.local>
2015-03-13 23:00:54 +0900

    TAJO-1400: Add TajoStatement::setMaxRows method support

--- tajo-jdbc/src/main/java/org/apache/tajo/jdbc/TajoStatement.java
+++ tajo-jdbc/src/main/java/org/apache/tajo/jdbc/TajoStatement.java
@@ -244,7 +244,7 @@ public class TajoStatement implements Statement {
 
   @Override
   public int getMaxRows() throws SQLException {
-    throw new SQLFeatureNotSupportedException("getMaxRows not supported");
+    return tajoClient.getMaxRows() ;
   }
 
   @Override
@@ -335,7 +335,10 @@ public class TajoStatement implements Statement {
 
   @Override
   public void setMaxRows(int max) throws SQLException {
-    throw new SQLFeatureNotSupportedException("setMaxRows not supported");
+	  if (max < 0) {
+	     throw new SQLException("max must be >= 0");
+	  }
+	  tajoClient.setMaxRows(max);
   }
 
   @Override
