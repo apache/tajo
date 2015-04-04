047f50eba66e719b24ad7f1210fb1379176e9501
Jihoon Son <jihoonson@apache.org>
2015-04-05 00:28:29 +0900

    TAJO-1344

--- tajo-plan/src/main/java/org/apache/tajo/plan/expr/PythonFunctionInvoke.java
+++ tajo-plan/src/main/java/org/apache/tajo/plan/expr/PythonFunctionInvoke.java
@@ -1,98 +1,98 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.apache.tajo.plan.expr;
-
-import com.google.common.base.Objects;
-import com.google.gson.annotations.Expose;
-import org.apache.tajo.OverridableConf;
-import org.apache.tajo.catalog.FunctionDesc;
-import org.apache.tajo.catalog.Schema;
-import org.apache.tajo.common.TajoDataTypes;
-import org.apache.tajo.datum.Datum;
-import org.apache.tajo.function.PythonInvocationDesc;
-import org.apache.tajo.plan.function.python.JythonScriptEngine;
-import org.apache.tajo.plan.function.python.JythonUtils;
-import org.apache.tajo.storage.Tuple;
-import org.apache.tajo.util.TUtil;
-import org.python.core.PyFunction;
-import org.python.core.PyObject;
-
-import java.io.IOException;
-import java.util.Arrays;
-
-public class PythonFunctionInvoke extends FunctionInvoke {
-  @Expose private PythonInvocationDesc invokeDesc;
-  @Expose private TajoDataTypes.DataType[] paramTypes;
-
-  public PythonFunctionInvoke(FunctionDesc funcDesc) {
-    super(funcDesc);
-    this.invokeDesc = funcDesc.getInvocation().getPython();
-    this.paramTypes = funcDesc.getSignature().getParamTypes();
-  }
-
-  @Override
-  public void init(OverridableConf queryContext, FunctionEval.ParamType[] paramTypes) {
-    // nothing to do
-  }
-
-  @Override
-  public Datum eval(Tuple tuple) {
-    try {
-      PyFunction function = JythonScriptEngine.getFunction(invokeDesc.getPath(), invokeDesc.getName());
-
-      PyObject result;
-      if (paramTypes.length == 0) {
-        result = function.__call__();
-      } else {
-        // Find the actual data types from the given parameters at runtime,
-        // and convert them into PyObject instances.
-        PyObject[] pyParams = JythonUtils.tupleToPyTuple(tuple).getArray();
-        result = function.__call__(pyParams);
-      }
-
-      return JythonUtils.pyObjectToDatum(result);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o instanceof PythonFunctionInvoke) {
-      PythonFunctionInvoke other = (PythonFunctionInvoke) o;
-      return this.invokeDesc.equals(other.invokeDesc) &&
-          TUtil.checkEquals(this.paramTypes, other.paramTypes);
-    }
-    return false;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(invokeDesc, Arrays.hashCode(paramTypes));
-  }
-
-  @Override
-  public Object clone() throws CloneNotSupportedException {
-    PythonFunctionInvoke clone = (PythonFunctionInvoke) super.clone();
-    clone.invokeDesc = (PythonInvocationDesc) this.invokeDesc.clone();
-    clone.paramTypes = new TajoDataTypes.DataType[paramTypes.length];
-    paramTypes = Arrays.copyOf(paramTypes, paramTypes.length);
-    return clone;
-  }
-}
+///**
+// * Licensed to the Apache Software Foundation (ASF) under one
+// * or more contributor license agreements.  See the NOTICE file
+// * distributed with this work for additional information
+// * regarding copyright ownership.  The ASF licenses this file
+// * to you under the Apache License, Version 2.0 (the
+// * "License"); you may not use this file except in compliance
+// * with the License.  You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package org.apache.tajo.plan.expr;
+//
+//import com.google.common.base.Objects;
+//import com.google.gson.annotations.Expose;
+//import org.apache.tajo.OverridableConf;
+//import org.apache.tajo.catalog.FunctionDesc;
+//import org.apache.tajo.catalog.Schema;
+//import org.apache.tajo.common.TajoDataTypes;
+//import org.apache.tajo.datum.Datum;
+//import org.apache.tajo.function.PythonInvocationDesc;
+//import org.apache.tajo.plan.function.python.JythonScriptEngine;
+//import org.apache.tajo.plan.function.python.JythonUtils;
+//import org.apache.tajo.storage.Tuple;
+//import org.apache.tajo.util.TUtil;
+//import org.python.core.PyFunction;
+//import org.python.core.PyObject;
+//
+//import java.io.IOException;
+//import java.util.Arrays;
+//
+//public class PythonFunctionInvoke extends FunctionInvoke {
+//  @Expose private PythonInvocationDesc invokeDesc;
+//  @Expose private TajoDataTypes.DataType[] paramTypes;
+//
+//  public PythonFunctionInvoke(FunctionDesc funcDesc) {
+//    super(funcDesc);
+//    this.invokeDesc = funcDesc.getInvocation().getPython();
+//    this.paramTypes = funcDesc.getSignature().getParamTypes();
+//  }
+//
+//  @Override
+//  public void init(OverridableConf queryContext, FunctionEval.ParamType[] paramTypes) {
+//    // nothing to do
+//  }
+//
+//  @Override
+//  public Datum eval(Tuple tuple) {
+//    try {
+//      PyFunction function = JythonScriptEngine.getFunction(invokeDesc.getPath(), invokeDesc.getName());
+//
+//      PyObject result;
+//      if (paramTypes.length == 0) {
+//        result = function.__call__();
+//      } else {
+//        // Find the actual data types from the given parameters at runtime,
+//        // and convert them into PyObject instances.
+//        PyObject[] pyParams = JythonUtils.tupleToPyTuple(tuple).getArray();
+//        result = function.__call__(pyParams);
+//      }
+//
+//      return JythonUtils.pyObjectToDatum(result);
+//    } catch (IOException e) {
+//      throw new RuntimeException(e);
+//    }
+//  }
+//
+//  @Override
+//  public boolean equals(Object o) {
+//    if (o instanceof PythonFunctionInvoke) {
+//      PythonFunctionInvoke other = (PythonFunctionInvoke) o;
+//      return this.invokeDesc.equals(other.invokeDesc) &&
+//          TUtil.checkEquals(this.paramTypes, other.paramTypes);
+//    }
+//    return false;
+//  }
+//
+//  @Override
+//  public int hashCode() {
+//    return Objects.hashCode(invokeDesc, Arrays.hashCode(paramTypes));
+//  }
+//
+//  @Override
+//  public Object clone() throws CloneNotSupportedException {
+//    PythonFunctionInvoke clone = (PythonFunctionInvoke) super.clone();
+//    clone.invokeDesc = (PythonInvocationDesc) this.invokeDesc.clone();
+//    clone.paramTypes = new TajoDataTypes.DataType[paramTypes.length];
+//    paramTypes = Arrays.copyOf(paramTypes, paramTypes.length);
+//    return clone;
+//  }
+//}
---
+++ tajo-plan/src/main/java/org/apache/tajo/plan/function/python/PythonScriptEngine.java
@@ -0,0 +1,175 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tajo.plan.function.python;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.tajo.catalog.CatalogUtil;
+import org.apache.tajo.catalog.FunctionDesc;
+import org.apache.tajo.catalog.proto.CatalogProtos;
+import org.apache.tajo.common.TajoDataTypes;
+import org.apache.tajo.function.FunctionInvocation;
+import org.apache.tajo.function.FunctionSignature;
+import org.apache.tajo.function.FunctionSupplement;
+import org.apache.tajo.function.PythonInvocationDesc;
+import org.apache.tajo.util.TUtil;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class PythonScriptEngine extends TajoScriptEngine {
+
+  private static final Log log = LogFactory.getLog(PythonScriptEngine.class);
+
+  public static Set<FunctionDesc> registerFunctions(String path, String namespace) throws IOException {
+
+//    String command = pigContext.getProperties().getProperty(
+//        PigConfiguration.PIG_STREAMING_UDF_PYTHON_COMMAND, "python");
+    Set<FunctionDesc> functionDescs = TUtil.newHashSet();
+
+    String command = "python";
+    String fileName = path.substring(0, path.length() - ".py".length());
+    log.debug("Path: " + path + " FileName: " + fileName + " Namespace: " + namespace);
+//    File f = new File(path);
+//
+//    if (!f.canRead()) {
+//      throw new IOException("Can't read file: " + path);
+//    }
+//
+//    FileInputStream fin = new FileInputStream(f);
+    InputStream in = getScriptAsStream(path);
+    List<FuncInfo> functions = null;
+    try {
+      functions = getFunctions(in);
+    } finally {
+      in.close();
+    }
+    namespace = namespace == null ? "" : namespace + NAMESPACE_SEPARATOR;
+    for(FuncInfo funcInfo : functions) {
+      String alias = namespace + funcInfo.funcName;
+//      String execType = (pigContext.getExecType() == ExecType.LOCAL? "local" : "mapreduce");
+//      String isIllustrate = (Boolean.valueOf(pigContext.inIllustrator)).toString();
+      log.debug("Registering Function: " + alias);
+//      pigContext.registerFunction(alias,
+//          new FuncSpec("StreamingUDF",
+//              new String[] {
+//                  command,
+//                  fileName, name,
+//                  schemaString, schemaLineNumber,
+//                  execType, isIllustrate
+//              }));
+      TajoDataTypes.DataType returnType = CatalogUtil.newSimpleDataType(TajoDataTypes.Type.valueOf(funcInfo.returnType));
+      FunctionSignature signature = new FunctionSignature(CatalogProtos.FunctionType.UDF, funcInfo.funcName,
+          returnType, createParamTypes(funcInfo.paramNum));
+      FunctionInvocation invocation = new FunctionInvocation();
+      PythonInvocationDesc invocationDesc = new PythonInvocationDesc(funcInfo.funcName, path);
+      invocation.setPython(invocationDesc);
+      FunctionSupplement supplement = new FunctionSupplement();
+      functionDescs.add(new FunctionDesc(signature, invocation, supplement));
+    }
+    return functionDescs;
+  }
+
+  private static TajoDataTypes.DataType[] createParamTypes(int paramNum) {
+    TajoDataTypes.DataType[] paramTypes = new TajoDataTypes.DataType[paramNum];
+    for (int i = 0; i < paramNum; i++) {
+      paramTypes[i] = TajoDataTypes.DataType.newBuilder().setType(TajoDataTypes.Type.ANY).build();
+    }
+    return paramTypes;
+  }
+
+//  @Override
+//  protected Map<String, List<PigStats>> main(PigContext context,
+//                                             String scriptFile) throws IOException {
+//    log.warn("ScriptFile: " + scriptFile);
+//    registerFunctions(scriptFile, null, context);
+//    return getPigStatsMap();
+//  }
+
+  @Override
+  protected String getScriptingLang() {
+    return "streaming_python";
+  }
+
+  @Override
+  protected Map<String, Object> getParamsFromVariables() throws IOException {
+    throw new IOException("Unsupported Operation");
+  }
+
+  private static final Pattern pSchema = Pattern.compile("^\\s*\\W+outputType.*");
+  private static final Pattern pDef = Pattern.compile("^\\s*def\\s+(\\w+)\\s*.+");
+
+  private static class FuncInfo {
+    String returnType;
+    String funcName;
+    int paramNum;
+    int schemaLineNumber;
+
+    public FuncInfo(String returnType, String funcName, int paramNum, int schemaLineNumber) {
+      this.returnType = returnType.toUpperCase();
+      this.funcName = funcName;
+      this.paramNum = paramNum;
+      this.schemaLineNumber = schemaLineNumber;
+    }
+  }
+
+  // TODO: python parser must be improved.
+  private static List<FuncInfo> getFunctions(InputStream is) throws IOException {
+    List<FuncInfo> functions = TUtil.newList();
+    InputStreamReader in = new InputStreamReader(is, Charset.defaultCharset());
+    BufferedReader br = new BufferedReader(in);
+    String line = br.readLine();
+    String schemaString = null;
+    int lineNumber = 1;
+    int schemaLineNumber = -1;
+    while (line != null) {
+      if (pSchema.matcher(line).matches()) {
+        int start = line.indexOf("(") + 2; //drop brackets/quotes
+        int end = line.lastIndexOf(")") - 1;
+        schemaString = line.substring(start,end).trim();
+        schemaLineNumber = lineNumber;
+      } else if (pDef.matcher(line).matches()) {
+        int nameStart = line.indexOf("def ") + "def ".length();
+        int nameEnd = line.indexOf('(');
+        int signatureEnd = line.indexOf(')');
+        String[] params = line.substring(nameEnd+1, signatureEnd).split(",");
+        int paramNum;
+        if (params.length == 1) {
+          paramNum = params[0].equals("") ? 0 : 1;
+        } else {
+          paramNum = params.length;
+        }
+
+        String functionName = line.substring(nameStart, nameEnd).trim();
+        schemaString = schemaString == null ? "blob" : schemaString;
+        functions.add(new FuncInfo(schemaString, functionName, paramNum, schemaLineNumber));
+        schemaString = null;
+      }
+      line = br.readLine();
+      lineNumber++;
+    }
+    br.close();
+    in.close();
+    return functions;
+  }
+}
