f47febf28f954fe311fb022a5eca5ba7a460c608
Yongjin Choi <superpiggy@gmail.com>
2015-04-05 20:31:03 +0900

    TAJO-1421: remove a redundant test that depends on system environment

--- tajo-core/src/test/java/org/apache/tajo/engine/query/TestAlterTable.java
+++ tajo-core/src/test/java/org/apache/tajo/engine/query/TestAlterTable.java
@@ -51,9 +51,8 @@ public class TestAlterTable extends QueryTestCaseBase {
   }
 
   @Test
-  public final void testAlterTableSetProperty1() throws Exception {
-    List<String> createdNames = executeDDL("table2_ddl.sql", "table2.tbl", "ALTX");
-
+  public final void testAlterTableSetProperty() throws Exception {
+    executeDDL("table2_ddl.sql", "table2.tbl", "ALTX");
     ResultSet before_res = executeQuery();
     assertResultSet(before_res, "before_set_property_delimiter.result");
     cleanupQuery(before_res);
@@ -62,21 +61,6 @@ public class TestAlterTable extends QueryTestCaseBase {
 
     ResultSet after_res = executeQuery();
     assertResultSet(after_res, "after_set_property_delimiter.result");
-    cleanupQuery(after_res);
-  }
-
-  @Test
-  public final void testAlterTableSetProperty2() throws Exception {
-    List<String> createdNames = executeDDL("table3_ddl.sql", "table3.tbl", "ALTY");
-
-    ResultSet before_res = executeQuery();
-    assertResultSet(before_res, "before_set_property_timezone.result");
-    cleanupQuery(before_res);
-
-    executeDDL("alter_table_set_property_timezone.sql", null);
-
-    ResultSet after_res = executeQuery();
-    assertResultSet(after_res, "after_set_property_timezone.result");
     cleanupQuery(after_res);
   }
 }
--- tajo-core/src/test/resources/queries/TestAlterTable/table3_ddl.sql
+++
@@ -1,1 +0,0 @@
-CREATE EXTERNAL TABLE ${0} (t_timestamp TIMESTAMP, t_date DATE) USING TEXT WITH('text.delimiter'='|', 'timezone'='ASIA/Seoul') LOCATION ${table.path};
