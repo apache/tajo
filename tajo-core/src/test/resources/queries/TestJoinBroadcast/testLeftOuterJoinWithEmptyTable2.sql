b8d70f11f2e6782f97c1ba85dd17fc87743fd273
Jihoon Son <jihoonson@apache.org>
2015-05-14 16:00:12 +0900

    Add result files

--- tajo-core/src/test/resources/queries/TestJoinBroadcast/testLeftOuterJoinWithEmptyTable2.sql
+++
@@ -1,9 +0,0 @@
-select
-  c_custkey,
-  sum(empty_orders.o_orderkey),
-  max(empty_orders.o_orderstatus),
-  max(empty_orders.o_orderdate)
-from
-  customer left outer join empty_orders on c_custkey = o_orderkey
-  group by c_custkey
-order by c_custkey ;
