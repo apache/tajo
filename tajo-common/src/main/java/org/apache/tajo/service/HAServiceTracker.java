04f03713fc78056b6f90b3edcfc157cf9e4967f7
Jinho Kim <jhkim@apache.org>
2015-05-15 10:28:23 +0900

    TAJO-1605: Fix master build failure on jdk 1.6

--- tajo-common/src/main/java/org/apache/tajo/service/HAServiceTracker.java
+++ tajo-common/src/main/java/org/apache/tajo/service/HAServiceTracker.java
@@ -21,10 +21,9 @@ package org.apache.tajo.service;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.net.NetUtils;
-import org.apache.tajo.conf.TajoConf;
-import org.apache.tajo.util.FileUtil;
 
 import javax.net.SocketFactory;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -56,10 +55,16 @@ public abstract class HAServiceTracker implements ServiceTracker {
 
       socket = socketFactory.createSocket();
       NetUtils.connect(socket, address, connectionTimeout);
-    } catch (Exception e) {
+    } catch (IOException e) {
       isAlive = false;
     } finally {
-      FileUtil.cleanup(LOG, socket);
+      if (socket != null) {
+        try {
+          socket.close();
+        } catch (IOException e) {
+          LOG.debug(e.getMessage(), e);
+        }
+      }
     }
     return isAlive;
   }
