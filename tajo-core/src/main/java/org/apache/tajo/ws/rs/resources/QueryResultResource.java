689c2ebde50822292d3fbb899dfbda6ab8e95a67
clark.kang <clark.kang@kakao.com>
2015-06-02 01:13:34 +0900

    fix error when offset is zero

--- tajo-core/src/main/java/org/apache/tajo/ws/rs/resources/QueryResultResource.java
+++ tajo-core/src/main/java/org/apache/tajo/ws/rs/resources/QueryResultResource.java
@@ -366,7 +366,7 @@ public class QueryResultResource {
     }
 
     private void skipOffsetRow(NonForwardQueryResultScanner queryResultScanner, int offset) throws IOException {
-      if (offset < 0) {
+      if (offset <= 0) {
         return;
       }
 
