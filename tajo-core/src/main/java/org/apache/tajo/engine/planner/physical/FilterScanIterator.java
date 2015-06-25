2ec307d621828cc18627ef7bd98e9c3c6774c5af
Hyunsik Choi <hyunsik@apache.org>
2015-06-24 20:51:07 -0700

    TAJO-1659: Simplify scan iteration in SeqScan.

---
+++ tajo-core/src/main/java/org/apache/tajo/engine/planner/physical/FilterScanIterator.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tajo.engine.planner.physical;
+
+import org.apache.tajo.plan.expr.EvalNode;
+import org.apache.tajo.storage.Scanner;
+import org.apache.tajo.storage.Tuple;
+
+import java.io.IOException;
+
+/**
+ * This iterator involves filter operation.
+ */
+public class FilterScanIterator implements ScanIterator {
+  private final Scanner scanner;
+  private final EvalNode filter;
+  private Tuple currentTuple;
+
+  public FilterScanIterator(Scanner scanner, EvalNode filter) {
+    this.scanner = scanner;
+    this.filter = filter;
+  }
+
+  @Override
+  public boolean hasNext() throws IOException {
+    while((currentTuple = scanner.next()) != null) {
+      if (filter.eval(currentTuple).isTrue()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public Tuple next() {
+    return currentTuple;
+  }
+}
+
