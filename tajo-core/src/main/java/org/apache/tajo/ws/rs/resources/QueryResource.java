90546191496a5c205bcb48d62ce68383061b05aa
clark.kang <clark.kang@kakao.com>
2015-07-04 02:01:05 +0900

    remove create table rest api interface
    remove tajo-env.sh

--- tajo-core/src/main/java/org/apache/tajo/ws/rs/resources/QueryResource.java
+++ tajo-core/src/main/java/org/apache/tajo/ws/rs/resources/QueryResource.java
@@ -18,6 +18,7 @@
 
 package org.apache.tajo.ws.rs.resources;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.tajo.QueryId;
@@ -34,6 +35,7 @@ import org.apache.tajo.session.Session;
 import org.apache.tajo.util.TajoIdUtils;
 import org.apache.tajo.ws.rs.*;
 import org.apache.tajo.ws.rs.requests.SubmitQueryRequest;
+import org.apache.tajo.ws.rs.responses.GetSubmitQueryResponse;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
@@ -261,19 +263,28 @@ public class QueryResource {
       }
       
       SubmitQueryResponse response = 
-          masterContext.getGlobalEngine().executeQuery(session, request.getQuery(), false);
+        masterContext.getGlobalEngine().executeQuery(session, request.getQuery(), false);
       if (response.hasResultCode() && ClientProtos.ResultCode.ERROR.equals(response.getResultCode())) {
         return ResourcesUtil.createExceptionResponse(LOG, response.getErrorMessage());
       } else {
         JerseyResourceDelegateContextKey<UriInfo> uriInfoKey =
-            JerseyResourceDelegateContextKey.valueOf(JerseyResourceDelegateUtil.UriInfoKey, UriInfo.class);
+          JerseyResourceDelegateContextKey.valueOf(JerseyResourceDelegateUtil.UriInfoKey, UriInfo.class);
         UriInfo uriInfo = context.get(uriInfoKey);
-        
+
+        QueryId queryId = new QueryId(response.getQueryId());
         URI queryURI = uriInfo.getBaseUriBuilder()
-            .path(QueryResource.class)
-            .path(QueryResource.class, "getQuery")
-            .build(new QueryId(response.getQueryId()).toString());
-        return Response.created(queryURI).build();
+          .path(QueryResource.class)
+          .path(QueryResource.class, "getQuery")
+          .build(queryId.toString());
+
+        GetSubmitQueryResponse queryResponse = new GetSubmitQueryResponse();
+        if (queryId.isNull() == false) {
+          queryResponse.setUri(queryURI);
+        }
+
+        queryResponse.setResultCode(response.getResultCode());
+        queryResponse.setQuery(request.getQuery());
+        return Response.status(Status.OK).entity(queryResponse).build();
       }
     }
   }
