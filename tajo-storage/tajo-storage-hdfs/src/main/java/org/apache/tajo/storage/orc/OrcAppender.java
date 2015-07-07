0e1bb5f7a1f992d2bf37f3fb1be3973906f36563
Jongyoung Park <eminency@gmail.com>
2015-07-07 17:48:10 +0900

    initial orc appender

---
+++ tajo-storage/tajo-storage-hdfs/src/main/java/org/apache/tajo/storage/orc/OrcAppender.java
@@ -0,0 +1,108 @@
+package org.apache.tajo.storage.orc;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.tajo.TaskAttemptId;
+import org.apache.tajo.catalog.Schema;
+import org.apache.tajo.catalog.TableMeta;
+import org.apache.tajo.catalog.statistics.TableStats;
+import org.apache.tajo.storage.FileAppender;
+import org.apache.tajo.storage.StorageConstants;
+import org.apache.tajo.storage.TableStatistics;
+import org.apache.tajo.storage.Tuple;
+import org.apache.tajo.storage.orc.objectinspector.ObjectInspectorFactory;
+import org.apache.tajo.storage.thirdparty.orc.CompressionKind;
+import org.apache.tajo.storage.thirdparty.orc.OrcFile;
+import org.apache.tajo.storage.thirdparty.orc.Writer;
+
+import java.io.IOException;
+
+public class OrcAppender extends FileAppender {
+  private Writer writer;
+  private TableStatistics stats;
+
+
+  public OrcAppender(Configuration conf, TaskAttemptId taskAttemptId, Schema schema,
+                      TableMeta meta, Path workDir) {
+    super(conf, taskAttemptId, schema, meta, workDir);
+  }
+
+  @Override
+  public void init() throws IOException {
+    writer = OrcFile.createWriter(workDir.getFileSystem(conf),
+      path, conf,
+      ObjectInspectorFactory.buildStructObjectInspector(schema),
+      Integer.parseInt(meta.getOption(StorageConstants.ORC_STRIPE_SIZE,
+        StorageConstants.DEFAULT_ORC_STRIPE_SIZE)), // default 1000
+      getCompressionKind(), // default 'none'
+      Integer.parseInt(meta.getOption(StorageConstants.ORC_BUFFER_SIZE,
+        StorageConstants.DEFAULT_ORC_BUFFER_SIZE)), // default 100
+      Integer.parseInt(meta.getOption(StorageConstants.ORC_ROW_INDEX_STRIDE,
+        StorageConstants.DEFAULT_ORC_ROW_INDEX_STRIDE))); // default 1000
+
+    if (enabledStats) {
+      this.stats = new TableStatistics(schema);
+    }
+
+    super.init();
+  }
+
+  @Override
+  public long getOffset() throws IOException {
+    return 0;
+  }
+
+  @Override
+  public void addTuple(Tuple tuple) throws IOException {
+    if (enabledStats) {
+      for (int i = 0; i < schema.size(); ++i) {
+        stats.analyzeField(i, tuple.get(i));
+      }
+    }
+    writer.addTuple(tuple);
+    if (enabledStats) {
+      stats.incrementRow();
+    }
+  }
+
+  @Override
+  public void flush() throws IOException {
+  }
+
+  @Override
+  public void close() throws IOException {
+    writer.close();
+  }
+
+  @Override
+  public TableStats getStats() {
+    if (enabledStats) {
+      return stats.getTableStat();
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public long getEstimatedOutputSize() throws IOException {
+    return writer.getRawDataSize() * writer.getNumberOfRows();
+  }
+
+  private CompressionKind getCompressionKind() {
+    String kindstr = meta.getOption(StorageConstants.ORC_COMPRESSION_KIND, StorageConstants.DEFAULT_ORC_COMPRESSION_KIND);
+
+    if (kindstr.equalsIgnoreCase(StorageConstants.ORC_COMPRESSION_KIND_ZIP)) {
+      return CompressionKind.ZLIB;
+    }
+
+    if (kindstr.equalsIgnoreCase(StorageConstants.ORC_COMPRESSION_KIND_SNAPPY)) {
+      return CompressionKind.SNAPPY;
+    }
+
+    if (kindstr.equalsIgnoreCase(StorageConstants.ORC_COMPRESSION_KIND_LZO)) {
+      return CompressionKind.LZO;
+    }
+
+    return CompressionKind.NONE;
+  }
+}
