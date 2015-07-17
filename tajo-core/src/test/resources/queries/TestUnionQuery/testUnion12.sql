92e3bc1b899036084bba29abe2ea82587cb76c12
Hyunsik Choi <hyunsik@apache.org>
2015-07-17 10:35:24 +0900

    TAJO-1514: Distinguish UNION and UNION ALL. (contributed by Keuntae Park, committed by hyunsik)
    Closes #505

--- tajo-core/src/test/resources/queries/TestUnionQuery/testUnion12.sql
+++ tajo-core/src/test/resources/queries/TestUnionQuery/testUnion12.sql
@@ -7,7 +7,8 @@ from (
             L_RETURNFLAG as col1, L_EXTENDEDPRICE as col2, concat(L_RECEIPTDATE, L_LINESTATUS) as col3
         from
             lineitem) b
-    union all
+    union
     select P_TYPE as col1, P_RETAILPRICE * 100 col2, concat('1993', P_NAME) col3 from part
 ) a
 where col3 like '1993%' and col2 > 46796
+order by col2;
