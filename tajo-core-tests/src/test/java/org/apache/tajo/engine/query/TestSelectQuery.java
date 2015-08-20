9a9f7c18d8bfaf9410d609cb4d9e75be4a48aa9f
Jongyoung Park <eminency@gmail.com>
2015-08-20 10:24:32 +0900

    unit text fixed

--- tajo-core-tests/src/test/java/org/apache/tajo/engine/query/TestSelectQuery.java
+++ tajo-core-tests/src/test/java/org/apache/tajo/engine/query/TestSelectQuery.java
@@ -675,7 +675,7 @@ public class TestSelectQuery extends QueryTestCaseBase {
 
     try {
       executeDDL("datetime_table_timezoned_ddl.sql", "timezoned", "timezoned_load1");
-      executeDDL("datetime_table_timezoned_ddl.sql", "temp", "timezoned_load2");
+      executeDDL("datetime_table_timezoned_ddl2.sql", null, "timezoned_load2");
       executeString("insert overwrite into timezoned_load2 select * from timezoned_load1");
 
       ResultSet res = executeQuery();
