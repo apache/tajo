fd8c6007e71bd7b897c772ef019195ad8b36b325
JaeHwa Jung <blrunner@apache.org>
2015-08-26 01:12:56 +0900

    Use tajo client APIs instead of catalog client APIs in test class.

--- tajo-core-tests/src/test/java/org/apache/tajo/master/TestNonForwardQueryResultSystemScanner.java
+++ tajo-core-tests/src/test/java/org/apache/tajo/master/TestNonForwardQueryResultSystemScanner.java
@@ -53,73 +53,30 @@ public class TestNonForwardQueryResultSystemScanner extends QueryTestCaseBase {
   public final void testGetInformationSchema() throws Exception {
     executeDDL("create_partitioned_table.sql", null);
 
+    ResultSet resultSet = null;
+    String actualResult, expectedResult;
     String simpleTableName = "information_schema_test_table";
-    String tableName = CatalogUtil.buildFQName(getCurrentDatabase(), simpleTableName);
-    assertTrue(catalog.existsTable(tableName));
-
-    TableDesc retrieved = catalog.getTableDesc(tableName);
 
     executeDDL("alter_table_add_partition1.sql", null);
     executeDDL("alter_table_add_partition2.sql", null);
 
-    List<CatalogProtos.DatabaseProto> allDatabases = catalog.getAllDatabases();
-    int dbId = -1;
-    for (CatalogProtos.DatabaseProto database : allDatabases) {
-      if (database.getName().equals(getCurrentDatabase())) {
-        dbId = database.getId();
-      }
+    resultSet = executeString("SELECT tid, table_name FROM INFORMATION_SCHEMA.TABLES "
+    + " WHERE TABLE_NAME = '" + simpleTableName + "'");
+    int tid = -1;
+    while (resultSet.next()) {
+      tid = resultSet.getInt("tid");
     }
-    assertNotEquals(dbId, -1);
+    resultSet.close();
+    assertTrue(tid > -1);
 
-    int tableId = -1;
-    List<CatalogProtos.TableDescriptorProto>  allTables = catalog.getAllTables();
-    for(CatalogProtos.TableDescriptorProto table : allTables) {
-      if (table.getDbId() == dbId && table.getName().equals(simpleTableName)) {
-        tableId = table.getTid();
-      }
+    int partitionId = -1;
+    resultSet = executeString("SELECT partition_id, partition_name FROM INFORMATION_SCHEMA.PARTITIONS "
+      + " WHERE tid = " + tid);
+    while (resultSet.next()) {
+      partitionId = resultSet.getInt("partition_id");
     }
-    assertNotEquals(tableId, -1);
-
-    List<CatalogProtos.PartitionDescProto> allPartitions = catalog.getAllPartitions();
-    List<CatalogProtos.PartitionDescProto> resultPartitions = TUtil.newList();
-
-    int partitionId = 0;
-    for (CatalogProtos.PartitionDescProto partition : allPartitions) {
-      if (partition.getTid() == tableId
-        && partition.getPartitionName().equals("col3=1/col4=2")
-        && partition.getPath().equals(retrieved.getUri().toString() + "/col3=1/col4=2")
-        ){
-        resultPartitions.add(partition);
-        partitionId = partition.getPartitionId();
-      }
-    }
-    assertEquals(resultPartitions.size(), 1);
-    assertEquals(resultPartitions.get(0).getPartitionName(), "col3=1/col4=2");
-
-    List<CatalogProtos.PartitionKeyProto> tablePartitionKeys = catalog.getAllPartitionKeys();
-    List<CatalogProtos.PartitionKeyProto> resultPartitionKeys = TUtil.newList();
-
-    for (CatalogProtos.PartitionKeyProto partitionKey: tablePartitionKeys) {
-      if (partitionKey.getPartitionId() == partitionId
-        && (partitionKey.getColumnName().equals("col3") && partitionKey.getPartitionValue().equals("1")
-        || partitionKey.getColumnName().equals("col4") && partitionKey.getPartitionValue().equals("2"))) {
-        resultPartitionKeys.add(partitionKey);
-      }
-    }
-    assertEquals(resultPartitionKeys.size(), 2);
-    assertEquals(resultPartitionKeys.get(0).getColumnName(), "col3");
-    assertEquals(resultPartitionKeys.get(0).getPartitionValue(), "1");
-    assertEquals(resultPartitionKeys.get(1).getColumnName(), "col4");
-    assertEquals(resultPartitionKeys.get(1).getPartitionValue(), "2");
-
-    ResultSet resultSet = executeString("SELECT partition_name FROM INFORMATION_SCHEMA.PARTITIONS "
-      + " WHERE partition_id = " + partitionId);
-
-    String actualResult = resultSetToString(resultSet);
-    String expectedResult = "partition_name\n" +
-      "-------------------------------\n" +
-      "col3=1/col4=2\n";
-    assertEquals(expectedResult, actualResult);
+    resultSet.close();
+    assertTrue(partitionId > -1);
 
     resultSet = executeString("SELECT column_name,partition_value FROM INFORMATION_SCHEMA.PARTITION_KEYS" +
       " WHERE partition_id = " + partitionId);
