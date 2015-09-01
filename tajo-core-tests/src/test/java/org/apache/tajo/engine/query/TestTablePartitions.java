1d85137ed34543e5e6f654fd9316840ef05051a2
JaeHwa Jung <blrunner@apache.org>
2015-09-02 01:17:58 +0900

    Remove unnecessary code

--- tajo-core-tests/src/test/java/org/apache/tajo/engine/query/TestTablePartitions.java
+++ tajo-core-tests/src/test/java/org/apache/tajo/engine/query/TestTablePartitions.java
@@ -71,7 +71,7 @@ public class TestTablePartitions extends QueryTestCaseBase {
     return Arrays.asList(new Object[][] {
       //type
       {NodeType.INSERT},
-//      {NodeType.CREATE_TABLE},
+      {NodeType.CREATE_TABLE},
     });
   }
 
