f90a3a5c44bc72dc97ed2628f2d6267061900e9a
Jihoon Son <jihoonson@apache.org>
2015-09-03 18:41:53 +0900

    TAJO-1811

--- tajo-catalog/tajo-catalog-server/src/test/java/org/apache/tajo/catalog/CatalogTestingUtil.java
+++ tajo-catalog/tajo-catalog-server/src/test/java/org/apache/tajo/catalog/CatalogTestingUtil.java
@@ -22,6 +22,7 @@ import org.apache.tajo.annotation.NotNull;
 import org.apache.tajo.annotation.Nullable;
 import org.apache.tajo.catalog.store.*;
 import org.apache.tajo.conf.TajoConf;
+import org.apache.tajo.conf.TajoConf.ConfVars;
 import org.apache.tajo.exception.UnsupportedCatalogStore;
 
 public class CatalogTestingUtil {
@@ -53,6 +54,7 @@ public class CatalogTestingUtil {
                                                                                String catalogUri) {
     conf.set(CatalogConstants.STORE_CLASS, catalogClass.getCanonicalName());
     conf.set(CatalogConstants.CATALOG_URI, catalogUri);
+    conf.setVar(ConfVars.CATALOG_ADDRESS, "localhost:0");
     return conf;
   }
 
