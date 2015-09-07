6c9ddeaac9513c7c332b9dc264e1c738edb796b4
Hyunsik Choi <hyunsik@apache.org>
2015-09-07 15:01:50 +0900

    TAJO-1816: Refactor SQL parser tests.
    Closes #731

---
+++ tajo-core-tests/src/test/resources/queries/TestSQLAnalyzer/positive/join_13.sql
@@ -0,0 +1,13 @@
+select * 
+from table1 
+cross join table2 
+join table3 on table1.id = table3.id
+inner join table4 on table1.id = table4.id 
+left outer join table5 on table1.id = table5.id 
+right outer join table6 on table1.id = table6.id 
+full outer join table7 on table1.id = table7.id 
+join table8 on table1.id = table8.id
+inner join table9 on table1.id = table9.id
+left outer join table10 on table1.id = table10.id
+right outer join table11 on table1.id = table11.id
+full outer join table12 on table1.id = table12.id
