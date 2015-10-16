bd7e20dda8c6c0efb1f079c4eaec1b65fb1c3372
JaeHwa Jung <blrunner@apache.org>
2015-10-16 17:30:39 +0900

    Implement setup and teardown

--- tajo-core-tests/src/test/java/org/apache/tajo/engine/planner/TestPartitionedTableRewriter.java
+++ tajo-core-tests/src/test/java/org/apache/tajo/engine/planner/TestPartitionedTableRewriter.java
@@ -25,33 +25,64 @@ import org.apache.tajo.OverridableConf;
 import org.apache.tajo.QueryTestCaseBase;
 import org.apache.tajo.algebra.Expr;
 import org.apache.tajo.catalog.CatalogUtil;
+import org.apache.tajo.catalog.Schema;
 import org.apache.tajo.catalog.TableDesc;
+import org.apache.tajo.catalog.TableMeta;
+import org.apache.tajo.catalog.partition.PartitionMethodDesc;
+import org.apache.tajo.catalog.proto.CatalogProtos;
+import org.apache.tajo.common.TajoDataTypes;
+import org.apache.tajo.conf.TajoConf;
 import org.apache.tajo.engine.query.QueryContext;
 import org.apache.tajo.plan.LogicalPlan;
 import org.apache.tajo.plan.logical.*;
 import org.apache.tajo.plan.rewrite.rules.PartitionedTableRewriter;
 import org.apache.tajo.util.CommonTestingUtil;
 import org.apache.tajo.util.FileUtil;
+import org.apache.tajo.util.KeyValueSet;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TestPartitionedTableRewriter extends QueryTestCaseBase {
 
-  @Test
-  public final void testPartitionPruningWithManualPartitionPathCreation() throws Exception {
-    String tableName = "testPartitionPruningUsingManualAddedPartition".toLowerCase();
-    String canonicalTableName = CatalogUtil.getCanonicalTableName("\"TestPartitionedTableRewriter\"", tableName);
+  final static String PARTITION_TABLE_NAME = "tb_partition";
+  final static String MULTIPLE_PARTITION_TABLE_NAME = "tb_multiple_partition";
 
-    executeString("create table " + canonicalTableName + " (n_nationkey INT8, n_name TEXT, n_regionkey INT8) " +
-      "partition by column(key TEXT)");
+  @BeforeClass
+  public static void setUp() throws Exception {
+    FileSystem fs = FileSystem.get(conf);
+    Path rootDir = TajoConf.getWarehouseDir(testingCluster.getConfiguration());
 
-    TableDesc tableDesc = catalog.getTableDesc(getCurrentDatabase(), tableName);
+    Schema schema = new Schema();
+    schema.addColumn("n_nationkey", TajoDataTypes.Type.INT8);
+    schema.addColumn("n_name", TajoDataTypes.Type.TEXT);
+    schema.addColumn("n_regionkey", TajoDataTypes.Type.INT8);
+
+    TableMeta meta = CatalogUtil.newTableMeta("TEXT", new KeyValueSet());
+
+    createPartitionTableIncludedOnePartitionKeyColumn(fs, rootDir, schema, meta);
+    createPartitionTableIncludedMultiplePartitionKeyColumns(fs, rootDir, schema, meta);
+  }
+
+  private static void createPartitionTableIncludedOnePartitionKeyColumn(FileSystem fs, Path rootDir, Schema schema,
+    TableMeta meta) throws Exception {
+    Schema partSchema = new Schema();
+    partSchema.addColumn("key", TajoDataTypes.Type.TEXT);
+
+    PartitionMethodDesc partitionMethodDesc =
+      new PartitionMethodDesc("TestPartitionedTableRewriter", PARTITION_TABLE_NAME,
+        CatalogProtos.PartitionType.COLUMN, "key", partSchema);
+
+    Path tablePath = new Path(rootDir, PARTITION_TABLE_NAME);
+    fs.mkdirs(tablePath);
+
+    client.createExternalTable(PARTITION_TABLE_NAME, schema, tablePath.toUri(), meta, partitionMethodDesc);
+
+    TableDesc tableDesc = client.getTableDesc(PARTITION_TABLE_NAME);
     assertNotNull(tableDesc);
 
-    FileSystem fs = FileSystem.get(conf);
     Path path = new Path(tableDesc.getUri().toString() + "/key=part123");
     fs.mkdirs(path);
     FileUtil.writeTextToFile("1|ARGENTINA|1", new Path(path, "data"));
@@ -63,17 +94,57 @@ public class TestPartitionedTableRewriter extends QueryTestCaseBase {
     path = new Path(tableDesc.getUri().toString() + "/key=part789");
     fs.mkdirs(path);
     FileUtil.writeTextToFile("3|CANADA|1", new Path(path, "data"));
-
-    testFilterIncludePartitionKeyColumn(canonicalTableName);
-    testWithoutAnyFilters(canonicalTableName);
-    testFilterIncludeNonExistingPartitionValue(canonicalTableName);
-    testFilterIncludeNonPartitionKeyColumn(canonicalTableName);
-
-    executeString("DROP TABLE " + canonicalTableName + " PURGE").close();
   }
 
-  private void testFilterIncludePartitionKeyColumn(String tableName) throws Exception {
-    Expr expr = sqlParser.parse("SELECT * FROM " + tableName + " WHERE key = 'part456' ORDER BY key");
+  private static void createPartitionTableIncludedMultiplePartitionKeyColumns(FileSystem fs, Path rootDir,
+      Schema schema, TableMeta meta) throws Exception {
+    Schema partSchema = new Schema();
+    partSchema.addColumn("key1", TajoDataTypes.Type.TEXT);
+    partSchema.addColumn("key2", TajoDataTypes.Type.TEXT);
+    partSchema.addColumn("key3", TajoDataTypes.Type.INT8);
+
+    PartitionMethodDesc partitionMethodDesc =
+      new PartitionMethodDesc("TestPartitionedTableRewriter", MULTIPLE_PARTITION_TABLE_NAME,
+        CatalogProtos.PartitionType.COLUMN, "key1,key2,key3", partSchema);
+
+    Path tablePath = new Path(rootDir, MULTIPLE_PARTITION_TABLE_NAME);
+    fs.mkdirs(tablePath);
+
+    client.createExternalTable(MULTIPLE_PARTITION_TABLE_NAME, schema, tablePath.toUri(), meta, partitionMethodDesc);
+
+    TableDesc tableDesc = client.getTableDesc(MULTIPLE_PARTITION_TABLE_NAME);
+    assertNotNull(tableDesc);
+
+    Path path = new Path(tableDesc.getUri().toString() + "/key1=part123");
+    fs.mkdirs(path);
+    path = new Path(tableDesc.getUri().toString() + "/key1=part123/key2=supp123");
+    fs.mkdirs(path);
+    path = new Path(tableDesc.getUri().toString() + "/key1=part123/key2=supp123/key3=1");
+    fs.mkdirs(path);
+    FileUtil.writeTextToFile("1|ARGENTINA|1", new Path(path, "data"));
+
+    path = new Path(tableDesc.getUri().toString() + "/key1=part123/key2=supp123/key3=2");
+    fs.mkdirs(path);
+    FileUtil.writeTextToFile("2|BRAZIL|1", new Path(path, "data"));
+
+    path = new Path(tableDesc.getUri().toString() + "/key1=part789");
+    fs.mkdirs(path);
+    path = new Path(tableDesc.getUri().toString() + "/key1=part789/key2=supp789");
+    fs.mkdirs(path);
+    path = new Path(tableDesc.getUri().toString() + "/key1=part789/key2=supp789/key3=3");
+    fs.mkdirs(path);
+    FileUtil.writeTextToFile("3|CANADA|1", new Path(path, "data"));
+  }
+
+  @AfterClass
+  public static void tearDown() throws Exception {
+    client.executeQuery("DROP TABLE IF EXISTS " + PARTITION_TABLE_NAME + " PURGE;");
+    client.executeQuery("DROP TABLE IF EXISTS " + MULTIPLE_PARTITION_TABLE_NAME + " PURGE;");
+  }
+
+  @Test
+  public void testFilterIncludePartitionKeyColumn() throws Exception {
+    Expr expr = sqlParser.parse("SELECT * FROM " + PARTITION_TABLE_NAME + " WHERE key = 'part456' ORDER BY key");
     QueryContext defaultContext = LocalTajoTestingUtility.createDummyContext(testingCluster.getConfiguration());
     LogicalPlan newPlan = planner.createPlan(defaultContext, expr);
     LogicalNode plan = newPlan.getRootBlock().getRoot();
@@ -104,8 +175,9 @@ public class TestPartitionedTableRewriter extends QueryTestCaseBase {
     assertEquals("key=part456", filteredPaths[0].getName());
   }
 
-  private void testWithoutAnyFilters(String tableName) throws Exception {
-    Expr expr = sqlParser.parse("SELECT * FROM " + tableName + " ORDER BY key");
+  @Test
+  public void testWithoutAnyFilters() throws Exception {
+    Expr expr = sqlParser.parse("SELECT * FROM " + PARTITION_TABLE_NAME + " ORDER BY key");
     QueryContext defaultContext = LocalTajoTestingUtility.createDummyContext(testingCluster.getConfiguration());
     LogicalPlan newPlan = planner.createPlan(defaultContext, expr);
     LogicalNode plan = newPlan.getRootBlock().getRoot();
@@ -133,8 +205,9 @@ public class TestPartitionedTableRewriter extends QueryTestCaseBase {
     assertEquals("key=part789", filteredPaths[2].getName());
   }
 
-  private void testFilterIncludeNonExistingPartitionValue(String tableName) throws Exception {
-    Expr expr = sqlParser.parse("SELECT * FROM " + tableName + " WHERE key = 'part123456789'");
+  @Test
+  public void testFilterIncludeNonExistingPartitionValue() throws Exception {
+    Expr expr = sqlParser.parse("SELECT * FROM " + PARTITION_TABLE_NAME + " WHERE key = 'part123456789'");
     QueryContext defaultContext = LocalTajoTestingUtility.createDummyContext(testingCluster.getConfiguration());
     LogicalPlan newPlan = planner.createPlan(defaultContext, expr);
     LogicalNode plan = newPlan.getRootBlock().getRoot();
@@ -161,8 +234,9 @@ public class TestPartitionedTableRewriter extends QueryTestCaseBase {
     assertEquals(0, filteredPaths.length);
   }
 
-  private void testFilterIncludeNonPartitionKeyColumn(String tableName) throws Exception {
-    String sql = "SELECT * FROM " + tableName + " WHERE n_nationkey = 1";
+  @Test
+  public void testFilterIncludeNonPartitionKeyColumn() throws Exception {
+    String sql = "SELECT * FROM " + PARTITION_TABLE_NAME + " WHERE n_nationkey = 1";
     Expr expr = sqlParser.parse(sql);
     QueryContext defaultContext = LocalTajoTestingUtility.createDummyContext(testingCluster.getConfiguration());
     LogicalPlan newPlan = planner.createPlan(defaultContext, expr);
@@ -194,47 +268,9 @@ public class TestPartitionedTableRewriter extends QueryTestCaseBase {
   }
 
   @Test
-  public final void testPartitionPruningWithManualMultiplePartitionPathCreation() throws Exception {
-    String tableName = "testPartitionPruningWithManualMultiplePartitionPathCreation".toLowerCase();
-    String canonicalTableName = CatalogUtil.getCanonicalTableName("\"TestPartitionedTableRewriter\"", tableName);
-
-    executeString("create table " + canonicalTableName + " (n_nationkey INT8, n_name TEXT, n_regionkey INT8) " +
-      "partition by column(key1 TEXT, key2 TEXT, key3 int)");
-
-    TableDesc tableDesc = catalog.getTableDesc(getCurrentDatabase(), tableName);
-    assertNotNull(tableDesc);
-
-    FileSystem fs = FileSystem.get(conf);
-    Path path = new Path(tableDesc.getUri().toString() + "/key1=part123");
-    fs.mkdirs(path);
-    path = new Path(tableDesc.getUri().toString() + "/key1=part123/key2=supp123");
-    fs.mkdirs(path);
-    path = new Path(tableDesc.getUri().toString() + "/key1=part123/key2=supp123/key3=1");
-    fs.mkdirs(path);
-    FileUtil.writeTextToFile("1|ARGENTINA|1", new Path(path, "data"));
-
-    path = new Path(tableDesc.getUri().toString() + "/key1=part123/key2=supp123/key3=2");
-    fs.mkdirs(path);
-    FileUtil.writeTextToFile("2|BRAZIL|1", new Path(path, "data"));
-
-    path = new Path(tableDesc.getUri().toString() + "/key1=part789");
-    fs.mkdirs(path);
-    path = new Path(tableDesc.getUri().toString() + "/key1=part789/key2=supp789");
-    fs.mkdirs(path);
-    path = new Path(tableDesc.getUri().toString() + "/key1=part789/key2=supp789/key3=3");
-    fs.mkdirs(path);
-    FileUtil.writeTextToFile("3|CANADA|1", new Path(path, "data"));
-
-    testFilterIncludeEveryPartitionKeyColumn(canonicalTableName);
-    testFilterIncludeSomeOfPartitionKeyColumns(canonicalTableName);
-    testFilterIncludeNonPartitionKeyColumns(canonicalTableName);
-
-    executeString("DROP TABLE " + canonicalTableName + " PURGE").close();
-  }
-
-  private void testFilterIncludeEveryPartitionKeyColumn(String tableName) throws Exception {
-    Expr expr = sqlParser.parse("SELECT * FROM " + tableName + " WHERE key1 = 'part789' and key2 = 'supp789' " +
-      "and key3=3");
+  public void testFilterIncludeEveryPartitionKeyColumn() throws Exception {
+    Expr expr = sqlParser.parse("SELECT * FROM " + MULTIPLE_PARTITION_TABLE_NAME
+      + " WHERE key1 = 'part789' and key2 = 'supp789' and key3=3");
     QueryContext defaultContext = LocalTajoTestingUtility.createDummyContext(testingCluster.getConfiguration());
     LogicalPlan newPlan = planner.createPlan(defaultContext, expr);
     LogicalNode plan = newPlan.getRootBlock().getRoot();
@@ -264,9 +300,10 @@ public class TestPartitionedTableRewriter extends QueryTestCaseBase {
     assertEquals("key1=part789", filteredPaths[0].getParent().getParent().getName());
   }
 
-  private void testFilterIncludeSomeOfPartitionKeyColumns(String tableName) throws Exception {
-    Expr expr = sqlParser.parse("SELECT * FROM " + tableName + " WHERE key1 = 'part123' and key2 = 'supp123' " +
-      " order by n_nationkey");
+  @Test
+  public void testFilterIncludeSomeOfPartitionKeyColumns() throws Exception {
+    Expr expr = sqlParser.parse("SELECT * FROM " + MULTIPLE_PARTITION_TABLE_NAME
+      + " WHERE key1 = 'part123' and key2 = 'supp123' order by n_nationkey");
     QueryContext defaultContext = LocalTajoTestingUtility.createDummyContext(testingCluster.getConfiguration());
     LogicalPlan newPlan = planner.createPlan(defaultContext, expr);
     LogicalNode plan = newPlan.getRootBlock().getRoot();
@@ -304,9 +341,10 @@ public class TestPartitionedTableRewriter extends QueryTestCaseBase {
     assertEquals("key1=part123", filteredPaths[1].getParent().getParent().getName());
   }
 
-  private void testFilterIncludeNonPartitionKeyColumns(String tableName) throws Exception {
-    Expr expr = sqlParser.parse("SELECT * FROM " + tableName + " WHERE key1 = 'part123' and n_nationkey >= 2 " +
-      " order by n_nationkey");
+  @Test
+  public void testFilterIncludeNonPartitionKeyColumns() throws Exception {
+    Expr expr = sqlParser.parse("SELECT * FROM " + MULTIPLE_PARTITION_TABLE_NAME
+      + " WHERE key1 = 'part123' and n_nationkey >= 2 order by n_nationkey");
     QueryContext defaultContext = LocalTajoTestingUtility.createDummyContext(testingCluster.getConfiguration());
     LogicalPlan newPlan = planner.createPlan(defaultContext, expr);
     LogicalNode plan = newPlan.getRootBlock().getRoot();
