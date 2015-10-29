782510e6022f4f45cbbdb88cb743e1f8e45396ba
Dongkyu Hwangbo <hwangbodk@gmail.com>
2015-10-29 15:25:58 +0900

    rollback

--- tajo-plan/src/main/java/org/apache/tajo/plan/logical/ProjectionNode.java
+++ tajo-plan/src/main/java/org/apache/tajo/plan/logical/ProjectionNode.java
@@ -18,7 +18,9 @@
 
 package org.apache.tajo.plan.logical;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.google.gson.annotations.Expose;
 
