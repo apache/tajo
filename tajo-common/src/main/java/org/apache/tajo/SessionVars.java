daac664f5e390b83410cdef8bfb0207ca142be5d
Jihoon Son <jihoonson@apache.org>
2015-11-09 15:30:07 +0900

    Fix test failure

--- tajo-common/src/main/java/org/apache/tajo/SessionVars.java
+++ tajo-common/src/main/java/org/apache/tajo/SessionVars.java
@@ -131,7 +131,7 @@ public enum SessionVars implements ConfigKey {
   NULL_CHAR(ConfVars.$TEXT_NULL, "null char of text file output", DEFAULT),
   CODEGEN(ConfVars.$CODEGEN, "Runtime code generation enabled (experiment)", DEFAULT),
   AGG_HASH_TABLE_SIZE(ConfVars.$AGG_HASH_TABLE_SIZE, "Aggregation hash table size", DEFAULT),
-  SORT_LIST_SIZE(ConfVars.$SORT_LIST_SIZE, "List size for in-memory sort ", DEFAULT),
+  SORT_LIST_SIZE(ConfVars.$SORT_LIST_SIZE, "List size for in-memory sort", DEFAULT),
   JOIN_HASH_TABLE_SIZE(ConfVars.$JOIN_HASH_TABLE_SIZE, "Join hash table size", DEFAULT),
 
   // for index
