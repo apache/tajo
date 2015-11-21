9d92e540c1d65b073508e5f783dd6d8358663408
JaeHwa Jung <blrunner@apache.org>
2015-11-22 00:33:00 +0900

    Recover partition paths in LogicalNode

--- tajo-plan/src/main/java/org/apache/tajo/plan/logical/PartitionedTableScanNode.java
+++ tajo-plan/src/main/java/org/apache/tajo/plan/logical/PartitionedTableScanNode.java
@@ -19,6 +19,8 @@
 package org.apache.tajo.plan.logical;
 
 import com.google.common.base.Objects;
+import com.google.gson.annotations.Expose;
+import org.apache.hadoop.fs.Path;
 import org.apache.tajo.catalog.TableDesc;
 import org.apache.tajo.plan.PlanString;
 import org.apache.tajo.plan.Target;
@@ -28,20 +30,31 @@ import org.apache.tajo.util.TUtil;
 import java.util.ArrayList;
 
 public class PartitionedTableScanNode extends ScanNode {
+  @Expose Path [] inputPaths;
+
   public PartitionedTableScanNode(int pid) {
     super(pid, NodeType.PARTITIONS_SCAN);
   }
 
-  public void init(ScanNode scanNode) {
+  public void init(ScanNode scanNode, Path[] inputPaths) {
     tableDesc = scanNode.tableDesc;
     setInSchema(scanNode.getInSchema());
     setOutSchema(scanNode.getOutSchema());
     this.qual = scanNode.qual;
     this.targets = scanNode.targets;
+    this.inputPaths = inputPaths;
 
     if (scanNode.hasAlias()) {
       alias = scanNode.alias;
     }
+  }
+
+  public void setInputPaths(Path [] paths) {
+    this.inputPaths = paths;
+  }
+
+  public Path [] getInputPaths() {
+    return inputPaths;
   }
 
 	public String toString() {
@@ -70,6 +83,7 @@ public class PartitionedTableScanNode extends ScanNode {
       eq = eq && TUtil.checkEquals(this.tableDesc, other.tableDesc);
       eq = eq && TUtil.checkEquals(this.qual, other.qual);
       eq = eq && TUtil.checkEquals(this.targets, other.targets);
+    eq = eq && TUtil.checkEquals(this.inputPaths, other.inputPaths);
 
       return eq;
     }
@@ -93,6 +107,8 @@ public class PartitionedTableScanNode extends ScanNode {
         unionScan.targets.add((Target) t.clone());
       }
     }
+
+    unionScan.inputPaths = inputPaths;
 
     return unionScan;
   }
@@ -131,6 +147,14 @@ public class PartitionedTableScanNode extends ScanNode {
 
     planStr.addDetail("out schema: ").appendDetail(getOutSchema().toString());
     planStr.addDetail("in schema: ").appendDetail(getInSchema().toString());
+
+    if (inputPaths != null) {
+      planStr.addExplan("num of filtered paths: ").appendExplain(""+ inputPaths.length);
+      int i = 0;
+      for (Path path : inputPaths) {
+        planStr.addDetail((i++) + ": ").appendDetail(path.toString());
+      }
+    }
 
     return planStr;
   }
