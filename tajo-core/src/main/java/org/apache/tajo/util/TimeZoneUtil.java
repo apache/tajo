1fac21a2e453a46d25869744df14d38f1be4eb0e
charsyam <charsyam@charsyamui-MacBook-Pro.local>
2015-11-28 13:33:07 +0900

    converted case sensative timezone

---
+++ tajo-core/src/main/java/org/apache/tajo/util/TimeZoneUtil.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tajo.util;
+
+import com.facebook.presto.hive.shaded.com.google.common.collect.Maps;
+import org.apache.tajo.SessionVars;
+import org.apache.tajo.storage.StorageConstants;
+
+import java.util.Map;
+
+public class TimeZoneUtil {
+  static Map<String, String> timeZoneIdMap;
+
+  static {
+    timeZoneIdMap = load();
+  }
+
+  private static Map<String, String> load() {
+    Map<String, String> timeZoneMap = Maps.newHashMap();
+    String[] timezoneIds = java.util.TimeZone.getAvailableIDs();
+    for (String timezoneId : timezoneIds) {
+      timeZoneMap.put(timezoneId.toUpperCase(), timezoneId);
+    }
+
+    return timeZoneMap;
+  }
+
+  private static String find(String timezoneId) {
+    if (timezoneId == null) {
+      return null;
+    }
+
+    return timeZoneIdMap.get(timezoneId.toUpperCase());
+  }
+
+  public static String getValidTimezone(String timeZoneId) {
+    String convertedTimezone = find(timeZoneId);
+    if (convertedTimezone != null) {
+      return convertedTimezone;
+    }
+
+    return timeZoneId;
+  }
+
+  public static boolean isTimezone(String key) {
+    return StorageConstants.TIMEZONE.equalsIgnoreCase(key);
+  }
+}
