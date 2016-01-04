3f4b2ce45fdb26b99c4e9b09118020091ce85df6
Dongkyu Hwangbo <hwangbodk@gmail.com>
2016-01-04 15:11:49 +0900

    rollback unnecessarily changed

--- tajo-pullserver/src/main/java/org/apache/tajo/pullserver/TajoPullServerService.java
+++ tajo-pullserver/src/main/java/org/apache/tajo/pullserver/TajoPullServerService.java
@@ -510,7 +510,7 @@ public class TajoPullServerService extends AbstractService {
         sendError(ctx, request.getDecoderResult().toString(), HttpResponseStatus.BAD_REQUEST);
         return;
       }
-      
+
       if (request.getMethod() == HttpMethod.DELETE) {
         HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NO_CONTENT);
         ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
