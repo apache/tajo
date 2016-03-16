ae707c82d7aca73ba58bd4288c40e6031e617202
Hyunsik Choi <hyunsik@apache.org>
2016-03-16 11:20:17 -0700

    TAJO-2043: Implement new data type and schema.
    Closes #934

---
+++ tajo-common/src/main/java/org/apache/tajo/type/Map.java
@@ -0,0 +1,66 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tajo.type;
+
+import org.apache.tajo.common.TajoDataTypes;
+
+import java.util.Objects;
+
+public class Map extends Type {
+  private final Type keyType;
+  private final Type valueType;
+
+  public Map(Type keyType, Type valueType) {
+    this.keyType = keyType;
+    this.valueType = valueType;
+  }
+
+  public Type keyType() {
+    return this.keyType;
+  }
+
+  public Type valueType() {
+    return this.valueType;
+  }
+
+  @Override
+  public TajoDataTypes.Type baseType() {
+    return TajoDataTypes.Type.MAP;
+  }
+
+  @Override
+  public String toString() {
+    return "map<" + keyType + "," + valueType + ">";
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(baseType(), keyType, valueType);
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (object instanceof Map) {
+      Map other = (Map) object;
+      return keyType.equals(other.keyType) && valueType.equals(other.valueType);
+    }
+
+    return false;
+  }
+}
