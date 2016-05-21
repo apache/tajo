98b99ac1f38255258038332a45079826ca096fb7
Janaka Chathuranga <bjchathuranga@gmail.com>
2016-05-21 14:33:33 +0530

    Created MongoDbFragment

--- tajo-storage/tajo-storage-mongodb/src/main/java/org/apache/tajo/storage/mongodb/MongoDbFragment.java
+++ tajo-storage/tajo-storage-mongodb/src/main/java/org/apache/tajo/storage/mongodb/MongoDbFragment.java
@@ -1,7 +1,40 @@
 package org.apache.tajo.storage.mongodb;
 
+import org.apache.tajo.catalog.proto.CatalogProtos;
+import org.apache.tajo.storage.fragment.Fragment;
+
 /**
  * Created by janaka on 5/21/16.
  */
-public class MongoDbFragment {
+public class MongoDbFragment implements Fragment {
+
+    @Override
+    public String getTableName() {
+        return null;
+    }
+
+    @Override
+    public CatalogProtos.FragmentProto getProto() {
+        return null;
+    }
+
+    @Override
+    public long getLength() {
+        return 0;
+    }
+
+    @Override
+    public String getKey() {
+        return null;
+    }
+
+    @Override
+    public String[] getHosts() {
+        return new String[0];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
