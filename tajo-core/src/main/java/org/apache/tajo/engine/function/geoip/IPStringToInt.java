41230497cf2e2c167d6bfc8be99dec51dfd86f63
Jongyoung Park <eminency@gmail.com>
2016-05-25 16:41:12 +0900

    ipstrToInt added

---
+++ tajo-core/src/main/java/org/apache/tajo/engine/function/geoip/IPStringToInt.java
@@ -0,0 +1,57 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.tajo.engine.function.geoip;
+
+import org.apache.tajo.catalog.Column;
+import org.apache.tajo.datum.Datum;
+import org.apache.tajo.datum.Int4Datum;
+import org.apache.tajo.datum.NullDatum;
+import org.apache.tajo.engine.function.annotation.Description;
+import org.apache.tajo.engine.function.annotation.ParamTypes;
+import org.apache.tajo.plan.function.GeneralFunction;
+import org.apache.tajo.storage.Tuple;
+import org.apache.tajo.util.GeoIPUtil;
+
+import static org.apache.tajo.common.TajoDataTypes.Type.INT4;
+import static org.apache.tajo.common.TajoDataTypes.Type.TEXT;
+
+@Description(
+    functionName = "ipstr_to_int",
+    description = "Convert an ipv4 address string to INT4 type",
+    example = "> SELECT ipstr_to_int('1.2.3.4');\n"
+        + "16909060",
+    returnType = INT4,
+    paramTypes = {@ParamTypes(paramTypes = {TEXT})}
+)
+public class IPStringToInt extends GeneralFunction {
+  public IPStringToInt() {
+    super(new Column[] { new Column("ipstring", TEXT)});
+  }
+
+  @Override
+  public Datum eval(Tuple params) {
+    if (params.isBlankOrNull(0)) {
+      return NullDatum.get();
+    }
+
+    String ipstr = params.getText(0);
+
+    return new Int4Datum(GeoIPUtil.ipstr2int(ipstr));
+  }
+}
